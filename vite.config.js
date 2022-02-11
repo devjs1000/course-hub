47c5eed3b3409d191ef5cd016452224503c9beb7
devjs1000 <atim4077@gmail.com>
2022-02-12 01:30:58 +0530

    vite config changed

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://management-xcitedu.herokuapp.com/",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+  plugins: [react()],
+
 })
