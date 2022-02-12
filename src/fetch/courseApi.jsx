72e96c7fd4f001ee6a2afec60fe435d0a6968795
devjs1000 <atim4077@gmail.com>
2022-02-12 11:59:41 +0530

    courserApi created

---
+++ src/fetch/courseApi.jsx
@@ -0,0 +1,53 @@
+import axios from "axios";
+
+export async function oneCourse(courseId, func) {
+  const { data } = await axios.get(`/api/course/oneCourse/${courseId}`);
+  func(data)
+}
+
+export async function allInstructorCourses(id, func) {
+  const { data } = await axios.get(`/api/course/allInstructorCourses/${id}`);
+  func(data)
+}
+
+export async function getAllCoursesOfUser(id, func) {
+  const { data } = await axios.get(`/api/order/getAllCoursesOfUser/${id}`);
+  func(data)
+}
+
+export async function allFrontendCourses(func) {
+  const { data } = await axios.get("/api/course/allFrontendCourses");
+  func(data)
+}
+
+export async function allBackendCourses(func) {
+  const { data } = await axios.get("/api/course/allBackendCourses");
+  func(data)
+}
+
+export async function allDesigningCourses(func) {
+  const { data } = await axios.get("/api/course/allDesigningCourses");
+  func(data)
+}
+
+export async function allFullstackCourses(func) {
+  const { data } = await axios.get("/api/course/allFullstackCourses");
+  func(data)
+}
+
+export async function allFullstackCourses(func) {
+  const { data } = await axios.get("/api/course/allFullstackCourses");
+  func(data)
+}
+
+export async function allOtherCourses(func) {
+  const { data } = await axios.get("/api/course/allOtherCourses");
+  func(data)
+}
+
+export async createCourse(instructorId, name, tagline, type, description, assignmentQuestion, imageLink, func){
+  const { data } = await axios.post(
+    "/api/course/createCourse",
+    {instructorId,name,tagline,type,description,assignmentQuestion,image: imageLink});
+    func(data)
+}
