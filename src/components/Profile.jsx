088d61fb97233f1fadac20f761e61454f1658f36
shakil89427 <shakilahmed89427@gmail.com>
2022-02-12 20:47:51 +0600

    shadow effect correction

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,15 +5,15 @@ const Profile = () => {
     const image = 'https://cdn.pixabay.com/photo/2020/05/11/15/38/tom-5158824_960_720.png'
 
     const profileDesign={
-        mainDiv:'grid grid-cols-12 gap-3 p-3 my-3 mx-3 md:mx-12',
-        leftMain:'col-span-12 md:col-span-4 lg:col-span-3  p-4 h-fit',
+        mainDiv:'grid grid-cols-12 gap-3 p-3 my-3 mx-3 md:mx-12 bg-slate-100',
+        leftMain:'col-span-12 md:col-span-4 lg:col-span-3  p-4 h-fit bg-slate-100',
         profileImg:'p-2 w-24 h-24 rounded-full block mx-auto shadow-2xl',
         profileName:'text-lg font-bold text-center mt-4 text-gray-800',
         editBtn:'ease-in duration-200 pb-1 border-2 border-red-700 rounded-md font-semibold text-md text-red-700 px-2 block mx-auto my-3 hover:bg-red-700 hover:text-white',
-        mainBtn:'ease-in duration-200 w-full flex items-center mt-3 hover:bg-red-700 hover:text-white py-1 rounded-lg shadow-2xl cursor-pointer',
+        mainBtn:'ease-in duration-200 w-full flex items-center mt-3 hover:bg-red-700 hover:text-white py-1 rounded-lg shadow-md cursor-pointer bg-white',
         mainIcon:'mx-2 text-red-700 w-8 h-8 p-1 bg-white rounded-full',
         mainBtnText:'text-md font-bold',
-        rightMain:'col-span-12 md:col-span-8 lg:col-span-9 shadow-2xl p-4 rounded-lg h-fit',
+        rightMain:'col-span-12 md:col-span-8 lg:col-span-9 shadow-lg p-4 rounded-lg h-fit bg-white',
         infoMain:'flex justify-between items-center border-b border-red-700 pb-2 text-md font-semibold px-3 mb-3',
     }
     
