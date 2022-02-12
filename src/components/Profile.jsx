ae2a4e68e4fd66c47eccb760bf40d09d9605a2b6
shakil89427 <shakilahmed89427@gmail.com>
2022-02-12 21:36:20 +0600

    icons updated and small changes

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Facebook, Github, Google, Headphones, LightbulbOff, Linkedin, Twitter } from 'react-bootstrap-icons';
+import { Activity, BoxArrowLeft, Journal, PatchCheck, Person,  Sliders, XDiamond } from 'react-bootstrap-icons';
 
 const Profile = () => {
     const image = 'https://cdn.pixabay.com/photo/2020/05/11/15/38/tom-5158824_960_720.png'
@@ -9,9 +9,9 @@ const Profile = () => {
         leftMain:'col-span-12 md:col-span-4 lg:col-span-3  p-4 h-fit bg-slate-100',
         profileImg:'p-2 w-24 h-24 rounded-full block mx-auto shadow-2xl',
         profileName:'text-lg font-bold text-center mt-4 text-gray-800',
-        editBtn:'ease-in duration-200 pb-1 border-2 border-red-700 rounded-md font-semibold text-md text-red-700 px-2 block mx-auto my-3 hover:bg-red-700 hover:text-white',
+        intro:'text-center text-md font-semibold',
         mainBtn:'ease-in duration-200 w-full flex items-center mt-3 hover:bg-red-700 hover:text-white py-1 rounded-lg shadow-md cursor-pointer bg-white',
-        mainIcon:'mx-2 text-red-700 w-8 h-8 p-1 bg-white rounded-full',
+        mainIcon:'mx-2 w-8 h-8 p-1',
         mainBtnText:'text-md font-bold',
         rightMain:'col-span-12 md:col-span-8 lg:col-span-9 shadow-lg p-4 rounded-lg h-fit bg-white',
         infoMain:'flex justify-between items-center border-b pb-2 text-md font-semibold px-3 mb-3',
@@ -25,40 +25,41 @@ const Profile = () => {
 
                 <p className={profileDesign.profileName}>Shakil Ahmed</p>
 
-                <button className={profileDesign.editBtn}>Edit Profile</button>
+                <p className={profileDesign.intro}>Student</p>
+                <p className={profileDesign.intro}>Joined at 10th July 2020</p>
 
                 <div className={profileDesign.mainBtn}>
-                <Facebook className={profileDesign.mainIcon}/>
-                <p className={profileDesign.mainBtnText}>Dashboard</p>
-                </div>
-
-                <div className={profileDesign.mainBtn}>
-                <Headphones className={profileDesign.mainIcon}/>
+                <Person className={profileDesign.mainIcon}/>
                 <p className={profileDesign.mainBtnText}>Profile</p>
                 </div>
 
                 <div className={profileDesign.mainBtn}>
-                <Twitter className={profileDesign.mainIcon}/>
+                <Sliders className={profileDesign.mainIcon}/>
                 <p className={profileDesign.mainBtnText}>Edit Profile</p>
                 </div>
 
                 <div className={profileDesign.mainBtn}>
-                <Linkedin className={profileDesign.mainIcon}/>
-                <p className={profileDesign.mainBtnText}>Affiliate</p>
-                </div>
-
-                <div className={profileDesign.mainBtn}>
-                <Google className={profileDesign.mainIcon}/>
+                <Journal className={profileDesign.mainIcon}/>
                 <p className={profileDesign.mainBtnText}>Courses</p>
                 </div>
 
                 <div className={profileDesign.mainBtn}>
-                <Github className={profileDesign.mainIcon}/>
-                <p className={profileDesign.mainBtnText}>Find More</p>
+                <PatchCheck className={profileDesign.mainIcon}/>
+                <p className={profileDesign.mainBtnText}>Verifications</p>
                 </div>
 
                 <div className={profileDesign.mainBtn}>
-                <LightbulbOff className={profileDesign.mainIcon}/>
+                <XDiamond className={profileDesign.mainIcon}/>
+                <p className={profileDesign.mainBtnText}>Reset Password</p>
+                </div>
+
+                <div className={profileDesign.mainBtn}>
+                <Activity className={profileDesign.mainIcon}/>
+                <p className={profileDesign.mainBtnText}>Activity Log</p>
+                </div>
+
+                <div className={profileDesign.mainBtn}>
+                <BoxArrowLeft className={profileDesign.mainIcon}/>
                 <p className={profileDesign.mainBtnText}>Logout</p>
                 </div>
 
