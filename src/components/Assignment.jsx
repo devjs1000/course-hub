551c8fb2bc90557d9df325d4a4da15f138f12948
devjs1000 <atim4077@gmail.com>
2022-02-13 22:55:18 +0530

    assignment ui improved

--- src/components/Assignment.jsx
+++ src/components/Assignment.jsx
@@ -4,9 +4,9 @@ import Search from '../UI/Search'
 import {ArrowClockwise} from 'react-bootstrap-icons';
 const Assignment = ({}) => {
   return (
-    <div className='bg-slate-100 h-[80vh]'>
-      <h1 className='p-4 text-4xl sm:text-6xl font-bold text-center text-slate-700 '>My Task Assignments</h1>
-      <div className='flex mx-[10rem]'>
+    <div className='bg-slate-100 h-[85vh]'>
+      <h1 className='p-4 text-4xl sm:text-6xl font-bold text-center text-slate-700 '>My Assignments</h1>
+      <div className='flex mx-[0rem] sm:mx-[2rem] sm:mx-[4rem] sm:mx-[6rem]'>
         <CountButton title={'All'} count={0} />
         <CountButton title={'Un Checked'} count={0} />
         <CountButton title={'Checked'} count={0} />
@@ -16,7 +16,7 @@ const Assignment = ({}) => {
 <ArrowClockwise className='bg-red-700 text-white w-[2rem] mx-1 hover:text-red-700 hover:bg-white transition-all h-[2rem] p-1' />
       </div>
 
-      <div className='h-[60vh] overflow-y-scroll'>
+      <div className='h-[70vh] overflow-y-scroll'>
 <div className='flex justify-between bg-white my-1 py-2 px-4 w-full items-center text-slate-700'>
 <div>Name of Task</div>
 <div>Course</div>
