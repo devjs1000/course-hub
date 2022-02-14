e9910bc2fc05c375f88fa99b0b71f6933b866d7a
mohit <mohitdalphonso@gmail.com>
2022-02-15 00:38:28 +0530

    code formatting

--- src/UI/Search.jsx
+++ src/UI/Search.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import {Search} from 'react-bootstrap-icons'
-const Assignment = ({value, placeholder, onChange, onSearch}) => {
-  return (
-  <div className='flex border items-center text-red-700 bg-white w-[16.5rem] hover:text-white hover:bg-red-700'>
-<input className='py-1 outline-red-700 text-slate-700 pl-2' placeholder={placeholder} value={value} onChange={onChange}/>
-<Search className='mx-2 transition-all h-[2rem]  w-[2rem]' onClick={onSearch}/>
-  </div>
-  );
-}
+import { Search } from 'react-bootstrap-icons';
 
+const Assignment = ({ value, placeholder, onChange, onSearch }) => {
+	return (
+		<div className="flex border items-center text-red-700 bg-white w-[16.5rem] hover:text-white hover:bg-red-700">
+			<input
+				className="py-1 outline-red-700 text-slate-700 pl-2"
+				placeholder={placeholder}
+				value={value}
+				onChange={onChange}
+			/>
+			<Search
+				className="mx-2 transition-all h-[2rem]  w-[2rem]"
+				onClick={onSearch}
+			/>
+		</div>
+	);
+};
 
 export default Assignment;
