bb00c4a123388fabee560b32fac83a334e694fd0
mohit <mohitdalphonso@gmail.com>
2022-02-15 21:13:16 +0530

    modification for testing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Header from '../components/header/Header';
 import CourseSections from '../components/courses/CourseSections';
+import { createCourse } from '../fetch/courseApi';
 
 const Home = () => {
 	return (
 		<>
 			<Header />
+			<createCourse />
 			<CourseSections />
 		</>
 	);
