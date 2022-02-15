100d81168c3cab501d740a091ebed0e59df1a0b9
mohit <mohitdalphonso@gmail.com>
2022-02-15 23:04:40 +0530

    form UI is updated

--- src/components/CreateCourse.jsx
+++ src/components/CreateCourse.jsx
@@ -1,146 +1,142 @@
-import React, {
-  useState
-} from 'react';
+import React, { useState } from 'react';
+import FormControl from '../UI/FormControl';
+import Button from '../UI/Button';
+
 const Form = () => {
-  const [basic, setBasic] = useState({})
-  const [advantage, setAdvantage] = useState({})
-  const [chapters, setChapters] = useState({})
-  const [quiz, setQuiz] = useState({})
-  const [image, setImage] = useState('')
-  const getBasic = e => {
-    const name = e.target.name
-    const value = e.target.value
-    const newData = {
-      ...basic
-    }
-    newData[name] = value
-    setBasic(newData)
-  }
-  const getAdvantage = e => {
-    const name = e.target.name
-    const value = e.target.value
-    const newData = {
-      ...advantage
-    }
-    newData[name] = value
-    setAdvantage(newData)
-  }
-  const getChapters = e => {
-    const name = e.target.name
-    const value = e.target.value
-    const newData = {
-      ...chapters
-    }
-    newData[name] = value
-    setChapters(newData)
-  }
-  const getQuiz = e => {
-    const name = e.target.name
-    const value = e.target.value
-    const newData = {
-      ...quiz
-    }
-    newData[name] = value
-    setQuiz(newData)
-  }
-  const getimg = e => {
-    const value = e.target.value
-    setImage(value)
-  }
-  const submitData = e => {
-    e.preventDefault()
-    const data = {
-      ...basic,
-      advantages: [advantage],
-      chapters: [chapters],
-      quiz: [quiz],
-      image
-    }
-    console.log(data)
-  }
-  return ( < div > < form className = 'flex flex-col h-screen justify-between'
-    onSubmit = {
-      submitData
-    } > < input onChange = {
-      getBasic
-    }
-    name = 'name'
-    placeholder = 'name'
-    type = "text" / > < input onChange = {
-      getBasic
-    }
-    name = 'tagline'
-    placeholder = 'tagline'
-    type = "text" / > < input onChange = {
-      getBasic
-    }
-    name = 'type'
-    placeholder = 'type'
-    type = "text" / > < input onChange = {
-      getBasic
-    }
-    name = 'assignmentQuestion'
-    placeholder = 'assignmentQuestion'
-    type = "text" / > < input onChange = {
-      getBasic
-    }
-    name = 'price'
-    placeholder = 'price'
-    type = "number" / > < input onChange = {
-      getAdvantage
-    }
-    name = 'advantageName'
-    placeholder = 'advantage name'
-    type = "text" / > < input onChange = {
-      getChapters
-    }
-    name = 'chaptername'
-    placeholder = 'chaptername'
-    type = "text" / > < input onChange = {
-      getChapters
-    }
-    name = 'chapternumber'
-    placeholder = 'chapternumber'
-    type = "number" / > < input onChange = {
-      getChapters
-    }
-    name = 'chapterVideoLink'
-    placeholder = 'chapterVideoLink'
-    type = "text" / > < input onChange = {
-      getChapters
-    }
-    name = 'chapterVideoDescription'
-    placeholder = 'chapterVideoDescription'
-    type = "text" / > < input onChange = {
-      getChapters
-    }
-    name = 'chapterStudyMaterial'
-    placeholder = 'chapterStudyMaterial'
-    type = "text" / > < input onChange = {
-      getQuiz
-    }
-    name = 'question'
-    placeholder = 'question'
-    type = "text" / > < input onChange = {
-      getQuiz
-    }
-    name = 'correct'
-    placeholder = 'correct'
-    type = "text" / > < input onChange = {
-      getQuiz
-    }
-    name = 'incorrect'
-    placeholder = 'incorrect'
-    type = "text" / > < input onChange = {
-      getimg
-    }
-    name = 'image'
-    placeholder = 'image link'
-    type = "text" / > < textarea onChange = {
-      getBasic
-    }
-    name = "description"
-    placeholder = 'description'
-    rows = "5" > < /textarea> < button type = 'submit' > Submit < /button> < /form> < /div>);
+	const [basic, setBasic] = useState({});
+	const [advantage, setAdvantage] = useState({});
+	const [chapters, setChapters] = useState({});
+	const [quiz, setQuiz] = useState({});
+	const [image, setImage] = useState('');
+	const getBasic = e => {
+		const name = e.target.name;
+		const value = e.target.value;
+		const newData = {
+			...basic,
+		};
+		newData[name] = value;
+		setBasic(newData);
+	};
+	const getAdvantage = e => {
+		const name = e.target.name;
+		const value = e.target.value;
+		const newData = {
+			...advantage,
+		};
+		newData[name] = value;
+		setAdvantage(newData);
+	};
+	const getChapters = e => {
+		const name = e.target.name;
+		const value = e.target.value;
+		const newData = {
+			...chapters,
+		};
+		newData[name] = value;
+		setChapters(newData);
+	};
+	const getQuiz = e => {
+		const name = e.target.name;
+		const value = e.target.value;
+		const newData = {
+			...quiz,
+		};
+		newData[name] = value;
+		setQuiz(newData);
+	};
+	const getimg = e => {
+		const value = e.target.value;
+		setImage(value);
+	};
+	const submitData = e => {
+		e.preventDefault();
+		const data = {
+			...basic,
+			advantages: [advantage],
+			chapters: [chapters],
+			quiz: [quiz],
+			image,
+		};
+		console.log(data);
+	};
+	return (
+		<div className="bg-primary-color-dark flex items-center justify-center py-4">
+			<div className="relative bg-white w-full mx-3 py-16 flex items-center justify-center rounded-xl sm:w-[60rem]">
+				<form className="w-4/5 h-full" onSubmit={submitData}>
+					<h2 className="text-center text-4xl font-semibold mb-12 sm:text-left">
+						Create Course
+					</h2>
+					<div className="flex flex-col items-start gap-4 mb-8 sm:gap-6">
+						<FormControl type="text" label="name" onChange={getBasic} />
+
+						<div className={groupClass}>
+							<FormControl type="text" label="tagline" onChange={getBasic} />
+							<FormControl type="text" label="type" onChange={getBasic} />
+						</div>
+						<FormControl
+							type="text"
+							label="assignmentQuestion"
+							onChange={getBasic}
+						/>
+						<div className={groupClass}>
+							<FormControl type="number" label="price" onChange={getBasic} />
+							<FormControl
+								type="text"
+								label="advantage"
+								onChange={getAdvantage}
+							/>
+						</div>
+
+						<FormControl
+							type="text"
+							label="chapter-name"
+							onChange={getChapters}
+						/>
+						<div className={groupClass}>
+							<FormControl
+								type="number"
+								label="chapter-number"
+								onChange={getChapters}
+							/>
+							<FormControl
+								type="text"
+								label="chapter-video-link"
+								onChange={getChapters}
+							/>
+						</div>
+						<div className={groupClass}>
+							<FormControl
+								type="text"
+								label="chapter-video-desc"
+								onChange={getChapters}
+							/>
+							<FormControl
+								type="text"
+								label="chapter-study-material"
+								onChange={getChapters}
+							/>
+						</div>
+
+						<FormControl type="text" label="question" onChange={getQuiz} />
+						<div className={groupClass}>
+							<FormControl type="text" label="correct" onChange={getQuiz} />
+							<FormControl type="text" label="incorrect" onChange={getQuiz} />
+						</div>
+
+						<FormControl type="text" label="image-link" onChange={getimg} />
+						<FormControl
+							type="textarea"
+							label="description"
+							onChange={getBasic}
+							rows="5"
+						/>
+					</div>
+					<Button type="submit" isWidthFull={true} isPrimary={true}>
+						Submit
+					</Button>
+				</form>
+			</div>
+		</div>
+	);
 };
 export default Form;
