a056553f1194b5f60c5d1dc49b3816084729c0ec
devjs1000 <atim4077@gmail.com>
2022-02-15 22:51:13 +0530

    user api created

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navigate, useLocation } from "react-router";
+import { Navigate, useLocation } from "react-router-dom";
 import useStore from '../context/useStore'
 import Loading from '../UI/Loading'
 
---
+++ src/fetch/userApi.jsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+export async function userRegister(name, email, isInstructor, isAdmin, mobileNumber, password , func) {
+  try{
+    const { data } = await axios.post(`/api/user/registerUser`, {name, email, isInstructor,password, isAdmin, mobileNumber});
+    func(data)
+  }catch(err){
+    func(data)
+  }
+}
+export async function userLogin(email, password , func) {
+  try{
+    const { data } = await axios.post(`/api/user/userLogin`, {email, password});
+    func(data)
+  }catch(err){
+    func(data)
+  }
+}
+export async function userDetails(userId , func) {
+  try{
+    const { data } = await axios.get(`/api/user/userDetails/${userId}`);
+    func(data)
+  }catch(err){
+    func(data)
+  }
+}
+export async function updateUserDetails(userId , func) {
+  try{
+    const { data } = await axios.post(`/api/user/userUpdate/${userId}`, {});
+    func(data)
+  }catch(err){
+    func(data)
+  }
+}
+
+export async function applyForInstructor( func) {
+  try{
+    const { data } = await axios.post(`/api/user/applyForInstructor, {});
+    func(data)
+  }catch(err){
+    func(data)
+  }
+}
