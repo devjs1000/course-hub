f3d080d6054b4738ee7b69c1e78b5bd321213d39
mohit <mohitdalphonso@gmail.com>
2022-02-15 23:08:08 +0530

    modified for test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,11 @@
 import Header from '../components/header/Header';
 import CourseSections from '../components/courses/CourseSections';
 import { createCourse } from '../fetch/courseApi';
-import CreateCourse from '../components/CreateCourse';
 
 const Home = () => {
 	return (
 		<>
 			<Header />
-			<CreateCourse />
 			<CourseSections />
 		</>
 	);
