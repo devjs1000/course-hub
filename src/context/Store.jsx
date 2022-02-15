597134e4dd0676ca48a7476b6762d1ae52909a32
devjs1000 <atim4077@gmail.com>
2022-02-16 02:02:23 +0530

    reference added of teacher data for development

--- src/context/Store.jsx
+++ src/context/Store.jsx
@@ -21,6 +21,26 @@ const Store = () => {
   useEffect(()=>{
     // authCheck(setUser,setUserLoading)
     const studentData={"success":true,"data":{"_id":"61d347889e68ba700bade96b","name":"Ram","email":"ram@gmail.com","password":"$2a$05$NtVsVdM6CDEqNo0mjCUBkedvEw509b.zwgvWjkkqSDf7.FlsIrWq6","isInstructor":false,"isAdmin":false,"appliedForInstructor":false,"profilePicture":"https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80","githubLink":"","linkedInLink":"","resumeLink":"https://d.novoresume.com/images/doc/functional-resume-template.png","mobileNumber":1111111111,"domains":"","description":"Describe yourself","createdAt":"2022-01-03T18:59:20.145Z","updatedAt":"2022-01-03T18:59:20.145Z","__v":0},"token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZDM0Nzg4OWU2OGJhNzAwYmFkZTk2YiIsImlhdCI6MTY0NDc1MjMxMH0.Vhrqz1X66NC8NqxIQW0xBszhBn7SfSLoCQKH0zILsPQ"}
+
+    const teacherData={
+    "_id": "61d326239e68ba700baddce4",
+    "name": "Adnan",
+    "email": "adnan@gmail.com",
+    "password": "$2a$05$8mZRtnYWMr5faGd4JpYG1OnidCO83SHySy.Nfu1/m7RJ5jjhZh0Xa",
+    "isInstructor": true,
+    "isAdmin": false,
+    "appliedForInstructor": false,
+    "profilePicture": "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80",
+    "githubLink": "",
+    "linkedInLink": "",
+    "resumeLink": "https://d.novoresume.com/images/doc/functional-resume-template.png",
+    "mobileNumber": 111111111111,
+    "domains": "",
+    "description": "Teacher with 20+ years of teaching experience and loves teaching",
+    "createdAt": "2022-01-03T16:36:52.180Z",
+    "updatedAt": "2022-01-03T16:36:52.180Z",
+    "__v": 0
+}
     setUser(studentData.data)
     setUserLoading(false)
   },[])
