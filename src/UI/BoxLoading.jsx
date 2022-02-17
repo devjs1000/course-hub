e1ecf6fab1322bb83286f264dc8ebfffec9328ba
mohit <mohitdalphonso@gmail.com>
2022-02-18 00:16:57 +0530

    modification in component for loader

---
+++ src/UI/BoxLoading.jsx
@@ -0,0 +1,14 @@
+import '../styles/PenLoading.css';
+
+const BoxLoading = () => {
+	return (
+		<div class="sk-folding-cube fixed left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] rotate-45">
+			<div class="sk-cube1 sk-cube"></div>
+			<div class="sk-cube2 sk-cube"></div>
+			<div class="sk-cube4 sk-cube"></div>
+			<div class="sk-cube3 sk-cube"></div>
+		</div>
+	);
+};
+
+export default BoxLoading;
