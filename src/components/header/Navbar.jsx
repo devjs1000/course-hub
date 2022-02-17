49339e45d287a8d5cfbac26b649f319254d2bf3e
mohit <mohitdalphonso@gmail.com>
2022-02-18 00:14:24 +0530

    repeatation of createPortal is removed

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,80 +1,85 @@
-import { useState } from "react";
-import { createPortal } from "react-dom";
-import logoPrimary from "../../images/logo-primary.png";
-import { List } from "react-bootstrap-icons";
-import Sidebar from "./Sidebar";
-import { Link, useLocation } from "react-router-dom";
-import useStore from "../../context/useStore";
+import { useState } from 'react';
+import { createPortal } from 'react-dom';
+import logoPrimary from '../../images/logo-primary.png';
+import { List } from 'react-bootstrap-icons';
+import Sidebar from './Sidebar';
+import { Link, useLocation } from 'react-router-dom';
+import useStore from '../../context/useStore';
+
+const Overlay = ({ openSidebarHandler }) => {
+	return <div className="overlay" onClick={openSidebarHandler}></div>;
+};
 
 function Navbar() {
-  const [openSidebar, setOpenSidebar] = useState(false);
-  const { user } = useStore();
+	const [openSidebar, setOpenSidebar] = useState(false);
+	const { user } = useStore();
 
-  const location = useLocation();
-  const path = location.pathname.split("/")[1];
-  //add objects in nullpath for hiding object in paths
-  const nullPath = ["login", "signup"];
-  if (nullPath.includes(path)) return null;
+	const location = useLocation();
+	const path = location.pathname.split('/')[1];
+	//add objects in nullpath for hiding object in paths
+	const nullPath = ['login', 'signup'];
+	if (nullPath.includes(path)) return null;
 
+	const openSidebarHandler = () => {
+		setOpenSidebar(!openSidebar);
+		document.getElementById('root').style.filter = 'blur(3px)';
+	};
 
-  const openSidebarHandler = () => {
-    setOpenSidebar(!openSidebar);
-    document.getElementById("root").style.filter = "blur(3px)";
-  };
+	return (
+		<nav className="flex gap-3 items-center  px-4 py-2 bg-white relative xsm:gap-5 md:flex-row md:px-16">
+			<List
+				className="w-8 cursor-pointer text-7xl"
+				onClick={openSidebarHandler}
+			></List>
 
-  return (
-    <nav className="flex gap-3 items-center  px-4 py-2 bg-white relative xsm:gap-5 md:flex-row md:px-16">
-      <List
-        className="w-8 cursor-pointer text-7xl"
-        onClick={openSidebarHandler}
-      ></List>
+			<Link to="/">
+				<img
+					src={logoPrimary}
+					alt="primary-logo-img"
+					className="w-36 lg:w-48"
+				/>
+			</Link>
 
-      <Link to="/">
-        <img
-          src={logoPrimary}
-          alt="primary-logo-img"
-          className="w-36 lg:w-48"
-        />
-      </Link>
-
-      <div className="flex items-center gap-4 ml-auto xsm:gap-6">
-        {user==undefined || !user._id  ? (
-          <>
-            <Link
-              to="/login"
-              className="link relative text-sm text-primary-color-dark font-semibold xsm:text-base"
-            >
-              Login?
-            </Link>
-            <Link to="/signup">
-              <button className="bg-red-700 text-white mx-2 px-2 py-1 rounded sm:px-4 sm:py-2">
-                signup
-              </button>
-            </Link>
-          </>
-        ) : (
-          <Link to='/my-profile'  className='flex gap-3 items-center'>
-            <span className="text-red-800 text-xl">{user.name}</span>
-            <img src={user.profilePicture} className='h-10 w-10 p-1 object-cover rounded-full'/>
-
-          </Link>
-        )}
-
-        {/* <List
-					className="w-8 cursor-pointer ml-4 text-7xl"
-					onClick={openSidebarHandler}
-				></List> */}
-      </div>
-      {createPortal(
-        <Sidebar
-          openSidebar={openSidebar}
-          user={user}
-          setOpenSidebar={setOpenSidebar}
-        />,
-        document.getElementById("sidebar")
-      )}
-    </nav>
-  );
+			<div className="flex items-center gap-4 ml-auto xsm:gap-6">
+				{user == undefined || !user._id ? (
+					<>
+						<Link
+							to="/login"
+							className="link relative text-sm text-primary-color-dark font-semibold xsm:text-base"
+						>
+							Login?
+						</Link>
+						<Link to="/signup">
+							<button className="bg-red-700 text-white mx-2 px-2 py-1 rounded sm:px-4 sm:py-2">
+								signup
+							</button>
+						</Link>
+					</>
+				) : (
+					<Link to="/my-profile" className="flex gap-3 items-center">
+						<span className="text-red-800 text-xl">{user.name}</span>
+						<img
+							src={user.profilePicture}
+							className="h-10 w-10 p-1 object-cover rounded-full"
+						/>
+					</Link>
+				)}
+			</div>
+			{createPortal(
+				<Sidebar
+					openSidebar={openSidebar}
+					user={user}
+					setOpenSidebar={setOpenSidebar}
+				/>,
+				document.getElementById('sidebar'),
+			)}
+			{openSidebar &&
+				createPortal(
+					<Overlay closeSidebarHandler={openSidebarHandler} />,
+					document.getElementById('overlay'),
+				)}
+		</nav>
+	);
 }
 
 export default Navbar;
--- src/components/header/Sidebar.jsx
+++ src/components/header/Sidebar.jsx
@@ -1,97 +1,87 @@
-import React from "react";
-import { createPortal } from "react-dom";
+import { Children } from 'react';
 import {
-  House,
-  Journal,
-  Book,
-  Person,
-  XLg,
-  People,
-} from "react-bootstrap-icons";
-import { Link } from "react-router-dom";
+	House,
+	Journal,
+	Book,
+	Person,
+	XLg,
+	People,
+} from 'react-bootstrap-icons';
+import { Link } from 'react-router-dom';
 
 function Sidebar({ openSidebar, setOpenSidebar, user }) {
-  let asideClasses = `fixed top-0 left-0 w-full  ${
-    openSidebar ? "transform translate-x-0" : "transform translate-x-[-100%]"
-  } h-full bg-white py-6 px-8 drop-shadow-lg transition ease-out duration-500 md:w-4/12 md:top-0 lg:w-1/4`;
+	let asideClasses = `fixed top-0 left-0 w-full  ${
+		openSidebar ? 'transform translate-x-0' : 'transform translate-x-[-100%]'
+	} h-full bg-white py-6 px-8 drop-shadow-lg transition ease-out duration-500 md:w-4/12 md:top-0 lg:w-1/4`;
 
-  const Overlay = ({ closeSidebarHandler }) => {
-    return <div className="overlay" onClick={closeSidebarHandler}></div>;
-  };
+	const closeSidebarHandler = () => {
+		setOpenSidebar(!openSidebar);
+		document.getElementById('root').style.filter = 'blur(0)';
+	};
 
-  const closeSidebarHandler = () => {
-    setOpenSidebar(!openSidebar);
-    document.getElementById("root").style.filter = "blur(0)";
-  };
+	const sideNav = [
+		{
+			name: 'Home',
+			icon: House,
+			path: '/',
+			log: true,
+		},
+		{
+			name: 'My Courses',
+			icon: Journal,
+			path: '/my-courses',
+			log: false,
+		},
+		{
+			name: 'My Assignments',
+			icon: Book,
+			path: '/my-assignments',
+			log: false,
+		},
+		{
+			name: 'Community',
+			icon: People,
+			path: '/community',
+			log: true,
+		},
+		{
+			name: 'My Profile',
+			icon: Person,
+			path: '/my-profile',
+			log: false,
+		},
+	];
 
-  const sideNav = [
-    {
-      name: "Home",
-      icon: House,
-      path: "/",
-      log: true,
-    },
-    {
-      name: "My Courses",
-      icon: Journal,
-      path: "/my-courses",
-      log: false,
-    },
-    {
-      name: "My Assignments",
-      icon: Book,
-      path: "/my-assignments",
-      log: false,
-    },
-    {
-      name: "Community",
-      icon: People,
-      path: "/community",
-      log: true,
-    },
-    {
-      name: "My Profile",
-      icon: Person,
-      path: "/my-profile",
-      log: false,
-    },
-  ];
-
-  const disabled = { pointerEvents: "none", opacity: ".5" };
-  return (
-    <aside className={asideClasses}>
-      <ul className="flex flex-col items-start gap-10 top trans">
-        <li className="self-end cursor-pointer">
-          <XLg
-            className="w-12 cursor-pointer hover:rotate-180 transition-all text-3xl"
-            onClick={closeSidebarHandler}
-          />
-        </li>
-        {React.Children.toArray(
-          sideNav.map((sidenav) => {
-            return (
-              <Link
-                style={user._id || sidenav.log ? {} : disabled}
-                to={sidenav.path}
-                className="flex items-center gap-2 hover:translate-x-2 hover:text-primary-color-dark transition-all"
-                onClick={closeSidebarHandler}
-              >
-                {<sidenav.icon className="text-lg" />}
-                <span href="#" className="text-lg">
-                  {sidenav.name}
-                </span>
-              </Link>
-            );
-          })
-        )}
-      </ul>
-      {openSidebar &&
-        createPortal(
-          <Overlay closeSidebarHandler={closeSidebarHandler} />,
-          document.getElementById("overlay")
-        )}
-    </aside>
-  );
+	const disabled = { pointerEvents: 'none', opacity: '.5' };
+	return (
+		<aside className={asideClasses}>
+			<ul className="flex flex-col items-start gap-10 top trans">
+				<li className="self-end cursor-pointer">
+					<XLg
+						className="w-12 cursor-pointer hover:rotate-180 transition-all text-3xl"
+						onClick={closeSidebarHandler}
+					/>
+				</li>
+				{Children.toArray(
+					sideNav.map(sidenav => {
+						return (
+							<Link
+								style={user._id || sidenav.log ? {} : disabled}
+								to={sidenav.path}
+								className="flex items-center gap-2 hover:translate-x-2 hover:text-primary-color-dark transition-all"
+								onClick={closeSidebarHandler}
+							>
+								{<sidenav.icon className="text-lg" />}
+								<span href="#" className="text-lg">
+									{sidenav.name}
+								</span>
+							</Link>
+						);
+					}),
+				)}
+			</ul>
+		</aside>
+	);
 }
 
 export default Sidebar;
