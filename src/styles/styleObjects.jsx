19caf49bb1b349cf97848a7a981c5356f9974ffc
mohit <mohitdalphonso@gmail.com>
2022-02-18 00:14:58 +0530

    unused styles are removed

--- src/styles/styleObjects.jsx
+++ src/styles/styleObjects.jsx
@@ -1,50 +1,44 @@
-export const footerStyles={
-  mainDiv:'bg-gray-900 p-3 md:p-8 relative',
-  gridDiv:'md:mx-14 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4',
-  title:'text-3xl font-bold text-white',
-  shortDes:'text-lg font-semibold text-white my-8 w-3/4',
-  listHead:'text-2xl font-semibold text-white',
-  listItem:'font-medium my-3 text-gray-400 hover:text-red-400 hover:ml-2 ease-linear duration-200 cursor-pointer w-fit',
-  contactItem: 'flex items-center my-3',
-  contactIcon:'bg-red-700 p-3 rounded-full mr-2 text-white',
-  contactMethod:'text-gray-400 text-base font-medium mb-1',
-  contactInfo:'text-lg font-bold text-white cursor-pointer',
-  bottomMain:'bg-black text-white md:mx-14 mt-8 flex flex-col-reverse justify-center md:flex-row md:justify-between items-center p-7',
-  copyright:'my-3 md:my-0 text-gray-400 text-base font-medium',
-  name:'text-red-600 cursor-pointer',
-  socialMain:'flex items-center',
-  socialIcon:'text-2xl mx-4 hover:text-red-400 cursor-pointer'
-}
+export const footerStyles = {
+	mainDiv: 'bg-gray-900 p-3 md:p-8 relative',
+	gridDiv: 'md:mx-14 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4',
+	title: 'text-3xl font-bold text-white',
+	shortDes: 'text-lg font-semibold text-white my-8 w-3/4',
+	listHead: 'text-2xl font-semibold text-white',
+	listItem:
+		'font-medium my-3 text-gray-400 hover:text-red-400 hover:ml-2 ease-linear duration-200 cursor-pointer w-fit',
+	contactItem: 'flex items-center my-3',
+	contactIcon: 'bg-red-700 p-3 rounded-full mr-2 text-white',
+	contactMethod: 'text-gray-400 text-base font-medium mb-1',
+	contactInfo: 'text-lg font-bold text-white cursor-pointer',
+	bottomMain:
+		'bg-black text-white md:mx-14 mt-8 flex flex-col-reverse justify-center md:flex-row md:justify-between items-center p-7',
+	copyright: 'my-3 md:my-0 text-gray-400 text-base font-medium',
+	name: 'text-red-600 cursor-pointer',
+	socialMain: 'flex items-center',
+	socialIcon: 'text-2xl mx-4 hover:text-red-400 cursor-pointer',
+};
 
-export const authDesign={
-screen:'h-screen flex items-center justify-center bg-red-800',
-mainDiv:'w-4/5 md:w-2/4 lg:w-2/6 border-2 bg-white  relative px-7 py-4',
-title:'mb-12 text-center font-bold text-3xl text-gray-600',
-itemDiv:'w-full flex items-center mb-5 pb-3 border-b-2',
-inputIcon:'text-gray-600 font-bold text-2xl',
-input:'w-full px-4 py-2 border-0 outline-none',
-forgotPass:'w-fit ml-auto my-1 cursor-pointer text-gray-500 font-semibold hover:text-red-500',
-mainBtn:'w-1/2 block mx-auto mt-7 py-2 rounded-3xl text-white font-bold text-md bg-gradient-to-r from-red-500 to-red-800 hover:bg-gradient-to-l from-red-500 to-orange-500',
-bottomText:'mt-8 text-center font-semibold text-gray-500 border-t-2',
-bottomIn:'ml-2 text-red-500 cursor-pointer',
-}
-
-
-export const profileDesign={
-    mainDiv:'grid grid-cols-12 gap-3 md:p-3 md:my-3 text-slate-800 md:mx-12 bg-red-700 md:bg-white',
-    leftMain:'col-span-12 md:col-span-5 lg:col-span-3 h-fit mx-3 md:pt-7',
-    person:'h-56 md:h-auto relative bg-white pb-2 rounded-bl-2xl rounded-br-2xl',
-    leftPersonBg:'md:hidden h-3/6 w-full',
-    profileImg:'absolute w-20 h-20 hover:p-2 transition-all object-cover bg-slate-200 p-1 top-1/4 left-0 right-0 md:static md:w-40 md:h-40 rounded-full block mx-auto',
-    leftInfoMain:'absolute bottom-2 left-0 right-0 md:static',
-    profileName:'text-lg font-bold text-center mt-2 text-slate-700',
-    intro:'text-center text-sm font-semibold text-slate-500',
-    editBtn:'block md:hidden mx-auto bg-red-700 text-white rounded-3xl px-3 py-1 text-xs mt-2',
-    mainBtnDiv:'bg-white p-1 my-3 rounded-lg',
-    mainBtn:'ease-in duration-200 flex items-center justify-between pl-2 pr-5 m-3 bg-white text-red-700 hover:bg-red-500 hover:text-white py-1 rounded-md  cursor-pointer bg-white',
-    activityH1:'text-2xl text-white md:text-gray-700 md:ml-5 pl-2 font-semibold',
-    active:'ease-in duration-200 flex items-center bg-gray-300 pl-5 mt-3 py-1 rounded-lg  cursor-pointer',
-    mainIcon:'mr-2 w-8 h-8 p-1',
-    mainBtnText:'text-md font-bold',
-    rightMain:'col-span-12 md:col-span-7 lg:col-span-9  rounded-lg h-fit bg-white m-3',
-}
+export const profileDesign = {
+	mainDiv:
+		'grid grid-cols-12 gap-3 md:p-3 md:my-3 text-slate-800 md:mx-12 bg-red-700 md:bg-white',
+	leftMain: 'col-span-12 md:col-span-5 lg:col-span-3 h-fit mx-3 md:pt-7',
+	person: 'h-56 md:h-auto relative bg-white pb-2 rounded-bl-2xl rounded-br-2xl',
+	leftPersonBg: 'md:hidden h-3/6 w-full',
+	profileImg:
+		'absolute w-20 h-20 hover:p-2 transition-all object-cover bg-slate-200 p-1 top-1/4 left-0 right-0 md:static md:w-40 md:h-40 rounded-full block mx-auto',
+	leftInfoMain: 'absolute bottom-2 left-0 right-0 md:static',
+	profileName: 'text-lg font-bold text-center mt-2 text-slate-700',
+	intro: 'text-center text-sm font-semibold text-slate-500',
+	editBtn:
+		'block md:hidden mx-auto bg-red-700 text-white rounded-3xl px-3 py-1 text-xs mt-2',
+	mainBtnDiv: 'bg-white p-1 my-3 rounded-lg',
+	mainBtn:
+		'ease-in duration-200 flex items-center justify-between pl-2 pr-5 m-3 bg-white text-red-700 hover:bg-red-500 hover:text-white py-1 rounded-md  cursor-pointer bg-white',
+	activityH1: 'text-2xl text-white md:text-gray-700 md:ml-5 pl-2 font-semibold',
+	active:
+		'ease-in duration-200 flex items-center bg-gray-300 pl-5 mt-3 py-1 rounded-lg  cursor-pointer',
+	mainIcon: 'mr-2 w-8 h-8 p-1',
+	mainBtnText: 'text-md font-bold',
+	rightMain:
+		'col-span-12 md:col-span-7 lg:col-span-9  rounded-lg h-fit bg-white m-3',
+};
