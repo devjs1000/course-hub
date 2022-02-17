76227ef58f17bc2001b4490b8df11e863749a4ad
mohit <mohitdalphonso@gmail.com>
2022-02-18 00:11:52 +0530

    new route to course details page is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect, useLayoutEffect, lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/header/Navbar';
-import Loading from './UI/PenLoading';
+import Loading from './UI/BoxLoading';
 import PrivateRoute from './components/PrivateRoute';
 import CreateCourse from './components/CreateCourse';
 
@@ -13,6 +13,7 @@ import MyCourses from './components/MyCourses';
 import Footer from './components/Footer';
 import Assignment from './components/Assignment';
 import Community from './components/Community/Community';
+import CourseDetails from './components/courseDetails/CourseDetails';
 
 // const Login =lazy(()=> import('./components/Auth/Login'))
 // const Signup =lazy(()=> import('./components/Auth/Signup'))
@@ -44,12 +45,22 @@ function App() {
 							<Route path="/" element={<Home />} />
 							<Route path="/login" element={<Login />} />
 							<Route path="/signup" element={<Signup />} />
-							<Route path="/my-profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
+							<Route
+								path="/my-profile"
+								element={
+									<PrivateRoute>
+										<Profile />
+									</PrivateRoute>
+								}
+							/>
 							<Route path="/my-courses" element={<MyCourses />} />
 							<Route path="/my-assignments" element={<Assignment />} />
 							<Route path="/Community" element={<Community />} />
 							<Route path="/create-course" element={<CreateCourse />} />
-
+							<Route
+								path="/coursedetails/:course"
+								element={<CourseDetails />}
+							/>
 						</Routes>
 						<Footer />
 					</Suspense>
