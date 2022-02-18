4824459ed84066fa777aa0ca9bff3d8178a3d6c4
mohit <mohitdalphonso@gmail.com>
2022-02-18 23:59:53 +0530

    connecting local props to the details page

--- src/pages/CourseDetails.jsx
+++ src/pages/CourseDetails.jsx
@@ -4,46 +4,83 @@ import CourseSkills from '../components/courseDetailsPage/CourseSkills';
 import HeroSection from '../components/courseDetailsPage/HeroSection';
 // import RelatedCourses from '../components/courseDetailsPage/RelatedCourses';
 import WhatYouGet from '../components/courseDetailsPage/WhatYouGet';
-import { oneCourse } from '../fetch/courseApi';
+// import { oneCourse } from '../fetch/courseApi';
 import { useParams } from 'react-router-dom';
-import useStore from '../context/useStore';
 import BoxLoading from '../UI/BoxLoading';
+import axios from 'axios';
 
 const CourseDetails = () => {
 	const [courseData, setCourseData] = useState({});
-
-	const {
-		frontendCourses,
-		backendCourses,
-		designingCourses,
-		fullstackCourses,
-		otherCourses,
-	} = useStore();
+	// const [courseInfo, setCourseInfo] = useState({});
+	const [isLoading, setIsLoading] = useState(true);
 
 	const param = useParams();
-	console.log(param);
 
 	useEffect(() => {
 		let isMount = true;
 
 		if (isMount) {
-			oneCourse(param.courseId, data => {
-				setCourseData(data.data);
-				console.log(courseData);
-			});
+			// oneCourse(param.courseId, data => {
+			// 	setCourseData(data.data);
+			// 	console.log(courseData);
+			// });
+			axios
+				.get('../../reference/course.json')
+				.then(res => {
+					res.data.map(course => {
+						Object.keys(course).map(() => {
+							course.data.map(courseDetails => {
+								if (courseDetails._id === param.courseId) {
+									setCourseData(courseData);
+								}
+							});
+						});
+					});
+				})
+				.catch(err => console.log(err))
+				.finally(() => {
+					setIsLoading(false);
+				});
 		}
 
 		return () => {
 			isMount = false;
 		};
 	}, []);
+
+	// !isLoading &&
+	// 	courseData.map(course => {
+	// 		Object.keys(course).map(() => {
+	// 			course.data.map(courseDetails => {
+	// 				if (courseDetails._id === param.courseId) {
+	// 					setCourseInfo(courseDetails);
+	// 				}
+	// 			});
+	// 		});
+	// 	});
+
+	const { name, tagline, type, price, advantages } = courseData;
+	console.log(courseData);
+
 	return (
 		<>
-			<HeroSection />
-			{/* <RelatedCourses /> */}
-			<WhatYouGet />
-			<CourseSkills />
-			<AuthorIntro />
+			{isLoading ? (
+				<BoxLoading />
+			) : (
+				<>
+					<HeroSection
+						name={name}
+						tagline={tagline}
+						type={type}
+						price={price}
+						advantages={advantages}
+					/>
+					{/* <RelatedCourses /> */}
+					<WhatYouGet />
+					<CourseSkills />
+					<AuthorIntro />
+				</>
+			)}
 		</>
 	);
 };
