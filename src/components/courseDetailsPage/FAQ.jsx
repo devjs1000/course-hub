f481bef6a626d09edc265162f76ad551753f68aa
mohit <mohitdalphonso@gmail.com>
2022-02-26 21:24:31 +0530

    FAQ section layout

---
+++ src/components/courseDetailsPage/FAQ.jsx
@@ -0,0 +1,11 @@
+function FAQ() {
+	return (
+		<section className="mt-24" id="#faq">
+			<h2 className="uppercase font-semibold text-lg">
+				Frequently Asked Questions
+			</h2>
+		</section>
+	);
+}
+
+export default FAQ;
