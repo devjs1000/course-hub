6a556ef8ccee3e866bd329006859023e7d939806
mohit <mohitdalphonso@gmail.com>
2022-02-26 19:37:03 +0530

    color changed for hover

--- src/components/courseDetailsPage/details/DetailsNavigation.jsx
+++ src/components/courseDetailsPage/details/DetailsNavigation.jsx
@@ -9,11 +9,11 @@ const navItems = [
 
 function DetailsNavigation() {
 	return (
-		<nav className=" bg-white px-16 sticky top-0 border-b z-50">
+		<nav className=" bg-[#f7f7f7] px-16 sticky top-0 z-50">
 			<ul className="flex items-center">
 				{Children.toArray(
 					navItems.map(item => (
-						<li className="hover:bg-primary-color-dark hover:text-white">
+						<li className="hover:bg-white">
 							<a href={item.href} className="inline-block text-md px-5 py-4">
 								{item.name}
 							</a>
