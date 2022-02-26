62b0afb404e8fd1abe5696da9d422221cbd01c11
mohit <mohitdalphonso@gmail.com>
2022-02-26 20:55:02 +0530

    UI of hero section of details page is changed and respective style is added

--- src/components/courseDetailsPage/HeroSection.jsx
+++ src/components/courseDetailsPage/HeroSection.jsx
@@ -1,37 +1,42 @@
-import { Children } from "react";
-import Button from "../../UI/Button";
-import sectionHeroImg from "../../images/course-details-hero.jpg";
+import { Children } from 'react';
+import Button from '../../UI/Button';
 
 function HeroSection({ course }) {
-  console.log(course);
+	console.log(course);
+	// { backgroundImage: `url(${course?.image})` }
+	return (
+		<div className="details-hero relative h-[60vh] overflow-hidden bg-center bg-fixed bg-no-repeat bg-cover">
+			<div className="absolute top-[50%] left-0 translate-y-[-50%] flex flex-col items-start sm:w-full backdrop-blur-[100px] px-16 py-8 sm:rounded-xl ">
+				<div
+					className="h-28 w-96 bg-cover bg-no-repeat bg-center rounded-md"
+					style={{ backgroundImage: `url(${course?.image})` }}
+				></div>
 
-  return (
-    <div
-      className="relative  h-auto   w-full overflow-hidden bg-center bg-fixed bg-no-repeat bg-cover flex justify-center items-center"
-      style={{ backgroundImage: `url(${course?.image})` }}
-    >
-      <div className="flex flex-wrap w-[30rem]  items-end h-[60vh] sm:w-full backdrop-blur-[100px] px-5 py-8  sm:rounded-xl ">
-        <div className="w-full">
-		<h1 className="text-4xl  w-full font-bold text-white px-4">
-          {course.name}
-        </h1>
-        <h2 className="text-xl  w-full text-white px-4">{course.tagline}</h2>
+				<div className="mb-7">
+					<h1 className="text-4xl font-bold text-white">{course.name}</h1>
+					<h2 className="text-xl text-white">{course.tagline}</h2>
+				</div>
 
+				<div className="">
+					<div className="my-4 flex items-center gap-2">
+						{Children.toArray(
+							course?.advantages?.map(a => {
+								return (
+									<span className="inline-block rounded-full bg-white px-4 py-1 text-slate-700">
+										{a.advantageName}
+									</span>
+								);
+							}),
+						)}
+					</div>
+					<div className="flex items-center gap-4 mt-4">
+						<Button isPrimary={true}>Enroll</Button>
+						<Button>Learn More</Button>
+					</div>
+				</div>
+			</div>
 		</div>
-
-        <div className=" gap-2 mx-4 ">
-          <Button isPrimary={true}>enroll</Button>
-          <Button>learn more</Button>
-		  <div className=" my-4 w-full">
-			{Children.toArray(course?.advantages?.map(a=>{
-				return <button className="bg-white m-1 px-2 text-slate-700">{a.advantageName}</button>
-			}))}
-		</div>
-
-        </div>
-      </div>
-    </div>
-  );
+	);
 }
 
 export default HeroSection;
