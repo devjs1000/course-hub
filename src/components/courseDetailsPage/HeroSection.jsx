9f92e3f0630306aa75612c3bf3a957e524bd9e4b
mohit <mohitdalphonso@gmail.com>
2022-02-26 21:19:28 +0530

    UI changed

--- src/components/courseDetailsPage/HeroSection.jsx
+++ src/components/courseDetailsPage/HeroSection.jsx
@@ -5,16 +5,13 @@ function HeroSection({ course }) {
 	console.log(course);
 	// { backgroundImage: `url(${course?.image})` }
 	return (
-		<div className="details-hero relative h-[60vh] overflow-hidden bg-center bg-fixed bg-no-repeat bg-cover">
-			<div className="absolute top-[50%] left-0 translate-y-[-50%] flex flex-col items-start sm:w-full backdrop-blur-[100px] px-16 py-8 sm:rounded-xl ">
-				<div
-					className="h-28 w-96 bg-cover bg-no-repeat bg-center rounded-md"
-					style={{ backgroundImage: `url(${course?.image})` }}
-				></div>
-
+		<div className="details-hero h-[60vh] overflow-hidden bg-center bg-fixed bg-no-repeat bg-cover flex justify-between items-center px-16">
+			<div className="flex flex-col items-start sm:w-full backdrop-blur-[100px] py-8 sm:rounded-xl ">
 				<div className="mb-7">
-					<h1 className="text-4xl font-bold text-white">{course.name}</h1>
-					<h2 className="text-xl text-white">{course.tagline}</h2>
+					<h1 className="text-4xl font-bold text-white uppercase tracking-wide">
+						{course.name}
+					</h1>
+					<p className="text-xl text-white">{course.tagline}</p>
 				</div>
 
 				<div className="">
@@ -29,12 +26,16 @@ function HeroSection({ course }) {
 							}),
 						)}
 					</div>
-					<div className="flex items-center gap-4 mt-4">
+					<div className="flex items-center gap-4 my-4">
 						<Button isPrimary={true}>Enroll</Button>
 						<Button>Learn More</Button>
 					</div>
 				</div>
 			</div>
+			<div
+				className="h-[80%] w-full bg-cover bg-no-repeat bg-center rounded-md relative after:absolute after:top-0 after:left-0 after:w-full after:h-full after:mix-blend-multiply after:bg-gray-600 after:opacity-75"
+				style={{ backgroundImage: `url(${course?.image})` }}
+			></div>
 		</div>
 	);
 }
