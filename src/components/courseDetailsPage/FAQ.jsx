93b684a91c3d2f9f6147e765f4092e1995be3a28
mohit <mohitdalphonso@gmail.com>
2022-02-28 19:21:34 +0530

    smooth scroling fixed

--- src/components/courseDetailsPage/FAQ.jsx
+++ src/components/courseDetailsPage/FAQ.jsx
@@ -2,7 +2,7 @@ import Accordion from './accordion/Accordion';
 
 function FAQ() {
 	return (
-		<section className="mt-24" id="#faq">
+		<section className="mt-24" id="faq">
 			<h2 className="uppercase font-semibold text-lg">
 				Frequently Asked Questions
 			</h2>
