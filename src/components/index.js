7ce56fe11b190afddd6d09492e02ed0592467649
mohit <mohitdalphonso@gmail.com>
2022-02-28 19:41:54 +0530

    all files are imported in same file for components folder

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import Assignment from './Assignment';
+import CreateAssignment from './CreateAssignment';
+import CreateCourse from './CreateCourse';
+import CreateQuiz from './CreateQuiz';
+import Footer from './Footer';
+import MyCourses from './MyCourses';
+import PrivateRoute from './PrivateRoute';
+import Profile from './Profile';
+import Tags from './Tags';
+
+export default {
+	Assignment,
+	CreateAssignment,
+	CreateCourse,
+	CreateQuiz,
+	Footer,
+	MyCourses,
+	PrivateRoute,
+	Profile,
+	Tags,
+};
