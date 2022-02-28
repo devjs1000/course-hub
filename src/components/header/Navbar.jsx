1e7f2aac62e6c58709cd10f408c02e2df74be78f
mohit <mohitdalphonso@gmail.com>
2022-02-28 23:27:09 +0530

    sticky navbar implemented

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import logoPrimary from '../../images/logo-primary.png';
 import { List } from 'react-bootstrap-icons';
@@ -12,10 +12,34 @@ const Overlay = ({ openSidebarHandler }) => {
 
 function Navbar() {
 	const [openSidebar, setOpenSidebar] = useState(false);
+	const [isVisible, setIsVisible] = useState();
 	const { user } = useStore();
+	const target = useRef();
+
+	console.log('navbar visibility', isVisible);
+
+	useEffect(() => {
+		const observer = new IntersectionObserver(
+			entries => {
+				const entry = entries[0];
+				setIsVisible(entry.isIntersecting);
+			},
+			{
+				root: null,
+				threshold: 0,
+				rootMargin: '64px',
+			},
+		);
+		observer.observe(target.current);
+
+		return () => {
+			observer.unobserve(target.current);
+		};
+	}, [isVisible]);
 
 	const location = useLocation();
 	const path = location.pathname.split('/')[1];
+
 	//add objects in nullpath for hiding object in paths
 	const nullPath = ['login', 'signup'];
 	if (nullPath.includes(path)) return null;
@@ -25,60 +49,69 @@ function Navbar() {
 		document.getElementById('root').style.filter = 'blur(3px)';
 	};
 
+	const initialNavClasses = `flex gap-3 items-center px-4 bg-white relative xsm:gap-5 md:flex-row md:px-16 transition-all duration-500`;
+
+	const navClasses = isVisible
+		? `${initialNavClasses}`
+		: `${initialNavClasses} sticky top-0 left-0 w-full h-16 bg-white shadow-md z-50 opacity-95 transition-all duration-500`;
+
 	return (
-		<nav className="flex gap-3 items-center px-4 bg-white relative xsm:gap-5 md:flex-row md:px-16">
-			<List
-				className="w-8 cursor-pointer text-7xl"
-				onClick={openSidebarHandler}
-			></List>
+		<>
+			<nav className={navClasses}>
+				<List
+					className="w-8 cursor-pointer text-7xl"
+					onClick={openSidebarHandler}
+				></List>
 
-			<Link to="/">
-				<img
-					src={logoPrimary}
-					alt="primary-logo-img"
-					className="w-36 lg:w-48"
-				/>
-			</Link>
+				<Link to="/">
+					<img
+						src={logoPrimary}
+						alt="primary-logo-img"
+						className="w-36 lg:w-48"
+					/>
+				</Link>
 
-			<div className="flex items-center gap-4 ml-auto xsm:gap-6">
-				{user == undefined || !user._id ? (
-					<>
-						<Link
-							to="/login"
-							className="link relative text-sm text-primary-color-dark font-semibold xsm:text-base"
-						>
-							Login?
+				<div className="flex items-center gap-4 ml-auto xsm:gap-6">
+					{user == undefined || !user._id ? (
+						<>
+							<Link
+								to="/login"
+								className="link relative text-sm text-primary-color-dark font-semibold xsm:text-base"
+							>
+								Login?
+							</Link>
+							<Link to="/signup">
+								<button className="bg-red-700 text-white mx-2 px-2 py-1 rounded sm:px-4 sm:py-2">
+									signup
+								</button>
+							</Link>
+						</>
+					) : (
+						<Link to="/my-profile" className="flex gap-3 items-center">
+							<span className="text-red-800 text-xl">{user.name}</span>
+							<img
+								src={user.profilePicture}
+								className="h-10 w-10 p-1 object-cover rounded-full"
+							/>
 						</Link>
-						<Link to="/signup">
-							<button className="bg-red-700 text-white mx-2 px-2 py-1 rounded sm:px-4 sm:py-2">
-								signup
-							</button>
-						</Link>
-					</>
-				) : (
-					<Link to="/my-profile" className="flex gap-3 items-center">
-						<span className="text-red-800 text-xl">{user.name}</span>
-						<img
-							src={user.profilePicture}
-							className="h-10 w-10 p-1 object-cover rounded-full"
-						/>
-					</Link>
+					)}
+				</div>
+				{createPortal(
+					<Sidebar
+						openSidebar={openSidebar}
+						user={user}
+						setOpenSidebar={setOpenSidebar}
+					/>,
+					document.getElementById('sidebar'),
 				)}
-			</div>
-			{createPortal(
-				<Sidebar
-					openSidebar={openSidebar}
-					user={user}
-					setOpenSidebar={setOpenSidebar}
-				/>,
-				document.getElementById('sidebar'),
-			)}
-			{openSidebar &&
-				createPortal(
-					<Overlay closeSidebarHandler={openSidebarHandler} />,
-					document.getElementById('overlay'),
-				)}
-		</nav>
+				{openSidebar &&
+					createPortal(
+						<Overlay closeSidebarHandler={openSidebarHandler} />,
+						document.getElementById('overlay'),
+					)}
+			</nav>
+			<div className="h-1 bg-transparent" ref={target}></div>
+		</>
 	);
 }
 
