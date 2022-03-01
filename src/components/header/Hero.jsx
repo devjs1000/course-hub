d45480bc15c6514e6534167e39fc80bb026b9454
mohit <mohitdalphonso@gmail.com>
2022-03-01 19:01:14 +0530

    new hero section layout

--- src/components/header/Hero.jsx
+++ src/components/header/Hero.jsx
@@ -1,37 +1,36 @@
-import React from "react";
-import heroImg from "../../images/hero-img.png";
-import Button from "../../UI/Button";
-import rectDesign from "../../images/rect-design.svg";
+import React from 'react';
+import Button from '../../UI/Button';
+import hero from '../../images/hero.png';
 
 function Hero() {
-  const bluePrint = {
-    heading:
-"Hold our hand on the path of your career.",    subHeading:
-"Take on the professional world by learning and practicing in-demand skills with us."  };
-  return (
-    <section className="section-hero z-10 bg-white flex flex-col gap-2 items-center px-4 py-4 relative sm:px-8 md:flex-row md:pt-0 lg:items-start lg:pt-12 xl:pt-16 xl:px-16">
-      <div className="flex flex-col items-center gap-4 order-2 xsm:gap-6 md:items-start md:w-1/2 lg:gap-10">
-        <h1 className="text-2xl text-center md:text-left lg:text-3xl xl:text-4xl">{bluePrint.heading}</h1>
-        <p className="w-full">{bluePrint.subHeading}</p>
-        <div className="flex items-center gap-4">
-          <Button isPrimary={true}>Start now</Button>
-          <Button isOutline={true} textPrimary={true}>
-            Find more
-          </Button>
-        </div>
-      </div>
-      <img
-        src={heroImg}
-        alt="image of excited girl"
-        className="w-full object-cover mr-16  md:order-2 md:mr-0 md:w-2/4 xl:w-2/4"
-      />
-      <img
-        src={rectDesign}
-        alt="rectangualr svg desgin for decoration"
-        className="absolute bottom-0 opacity-0 lg:w-60 lg:opacity-100 xl:w-auto"
-      />
-    </section>
-  );
+	const bluePrint = {
+		heading: 'Hold our hand on the path of your career.',
+		subHeading:
+			'Take on the professional world by learning and practicing in-demand skills with us.',
+	};
+
+	return (
+		<section className="h-[100vh] flex flex-col gap-2 items-center px-4 py-4 relative sm:px-8 md:flex-row md:pt-0  lg:pt-12 xl:pt-16 xl:px-16">
+			<div className="hero-inner absolute top-0 left-0 w-full h-full bg-red-400 z-[-1]"></div>
+			<div className="flex flex-col w-[50%] gap-6">
+				<h1 className="text-7xl font-bold">{bluePrint.heading}</h1>
+				<p className="text-xl">{bluePrint.subHeading}</p>
+				<div>
+					<button
+						className="border-none inline-block px-6 py-3 bg-violet-700 font-semibold text-lg text-white rounded
+          "
+					>
+						Enroll Now
+					</button>
+					<button>Learn More</button>
+				</div>
+			</div>
+			<div
+				className="h-full w-[50%] bg-cover"
+				style={{ backgroundImage: `url(${hero})` }}
+			></div>
+		</section>
+	);
 }
 
 export default Hero;
