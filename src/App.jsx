3f0489318730a3431e46c147251d3c52141111f0
Keshav <keshavkumar14092001@gmail.com>
2022-03-02 10:31:09 +0530

    Solving all the issues of Modal

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from './components/header/Navbar';
 import Loading from './UI/BoxLoading';
 import PrivateRoute from './components/PrivateRoute';
 import CreateCourse from './components/CreateCourse';
-import  CreateQuiz  from './components/CreateQuiz';
+import CreateQuiz from './components/CreateQuiz';
 import NotFound from './pages/NotFound';
 import Home from './pages/Home';
 import Login from './components/Auth/Login';
@@ -16,14 +16,20 @@ import Assignment from './components/Assignment';
 import Community from './components/Community/Community';
 import CourseDetails from './pages/CourseDetails';
 import CreateAssignment from './components/CreateAssignment';
+// import Modal from './components/Modal/Modal';
 
 
 function App() {
 	const [loading, setLoading] = useState(true);
-console.log(import.meta.env.VITE_DB);
+	console.log(import.meta.env.VITE_DB);
 	useLayoutEffect(() => {
 		setTimeout(() => setLoading(false), 1500);
 	}, []);
+
+	// Require Hooks and Functions For Modal:
+
+	// const [showModal, setShowModal] = useState(false);
+	// const closeBtnHandler = () => setShowModal(false)
 
 	return (
 		<>
@@ -63,6 +69,23 @@ console.log(import.meta.env.VITE_DB);
 					</Suspense>
 				</div>
 			)}
+			{/* Calling Modal */}
+			{/* <div>
+				{showModal ?
+					<div
+						className="back-drop fixed bg-[rgba(0,0,0,0.5)] h-full w-full transition-all"
+						onClick={() => closeBtnHandler()}
+					>
+					</div>
+					: null}
+				<button
+					className="btn-openModal bg-[#2c2c2c] text-[#eee] m-8 py-2 px-7 cursor-pointer text-lg"
+					onClick={() => setShowModal(true)}
+				>
+					Open Modal
+				</button>
+				<Modal status={showModal} closeBtnHandler={closeBtnHandler} />
+			</div> */}
 		</>
 	);
 }
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,77 +1,60 @@
-import React, { useState } from "react";
-import { BsXLg } from "react-icons/bs";
+import { FaUserAlt } from 'react-icons/fa';
+import { FaLock } from 'react-icons/fa';
 
-const Modal = () => {
-    const [showModal, setShowModal] = useState(false);
+const Modal = ({ status, closeBtnHandler }) => {
     return (
-        <>
-            <button
-                className="bg-blue-200 text-black active:bg-blue-500 
-                font-bold px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
-                type="button"
-                onClick={() => setShowModal(true)}
-            >
-                Fill Details
-            </button>
-            {showModal ? (
-                <>
-                    <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-                        <div className="relative my-6 mx-auto w-[40%]">
-                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                                <div className="flex items-center justify-between p-5 border-b border-solid border-gray-300 rounded-t ">
-                                    <h3 className="text-3xl font-semibold text-slate-800">General Info</h3>
-                                    <button
-                                        className="bg-transparent border-0 text-black float-right"
-                                        onClick={() => setShowModal(false)}
-                                    >
-                                        <span className="flex items-center justify-center text-black opacity-7 h-6 w-6 text-xl bg-slate-800 py-0 rounded-full hover:bg-red-500 transition">
-                                            <BsXLg className="p-[2px] font-bold text-white" />
-                                        </span>
-                                    </button>
-                                </div>
-                                <div className="relative p-6 flex-auto">
-                                    <form className="bg-gray-200 shadow-md rounded px-8 pt-6 pb-8 w-full">
-                                        <label className="block text-black text-sm font-bold mb-1">
-                                            First Name
-                                        </label>
-                                        <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black my-2" />
-                                        <label className="block text-black text-sm font-bold mb-1">
-                                            Last Name
-                                        </label>
-                                        <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black my-2" />
-                                        <label className="block text-black text-sm font-bold mb-1">
-                                            Address
-                                        </label>
-                                        <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black my-2" />
-                                        <label className="block text-black text-sm font-bold mb-1">
-                                            City
-                                        </label>
-                                        <input className="shadow appearance-none border rounded w-full py-2 px-1 text-black my-2" />
-                                    </form>
-                                </div>
-                                <div className="flex items-center justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
-                                    <button
-                                        className="text-white bg-red-500 active:bg-yellow-700 font-bold uppercase text-sm px-6 py-3 rounded shadow-md hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 hover:bg-red-600 transition"
-                                        type="button"
-                                        onClick={() => setShowModal(false)}
-                                    >
-                                        Close
-                                    </button>
-                                    <button
-                                        className="text-white bg-green-500 active:bg-yellow-700 font-bold uppercase text-sm px-6 py-3 rounded shadow-md hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 hover:bg-green-600 transition"
-                                        type="button"
-                                        onClick={() => setShowModal(false)}
-                                    >
-                                        Submit
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
+        <div
+            className="bg-white rounded-lg shadow-lg outline outline-offset-0 outline-[#9d0000] w-[80%] max-w-[800px] mx-auto mt-[5rem] transition-all duration-500"
+            style={{
+                transform: status ? 'translateY(-15vh)' : 'translateY(-50vh)',
+                opacity: status ? '1' : '0'
+            }}
+        >
+            <div className="modal-header bg-[#9d0000] text-white p-[1rem] text-3xl">
+                <p>Welcome To Our Site</p>
+            </div>
+            <div className="modal-content py-6 px-4">
+                <div className="modal-body mb-4">
+                    <div className="relative p-6 flex-auto">
+                        <form className="bg-gray-200 shadow-md rounded px-8 pt-6 pb-8 w-full">
+                            <label className="text-slate-800 text-base tracking-wide font-bold my-1 flex items-center justify-start">
+                                <span><FaUserAlt className='inline' /></span>
+                                <span className='pl-2'>Username</span>
+                            </label>
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-1 text-black mb-4 outline-none hover:outline-[#9d0000] hover:outline-offset-0 transition-all"
+                                type={"email"}
+                            />
+                            <label className="text-slate-800 text-base tracking-wide font-bold my-1 flex items-center justify-start">
+                                <span><FaLock className='inline' /></span>
+                                <span className='pl-2'>Password</span>
+                            </label>
+                            <input
+                                className="shadow appearance-none border rounded w-full py-2 px-1 text-black mb-4 outline-none hover:outline-[#9d0000] hover:outline-offset-0 transition-all"
+                                type={"password"}
+                            />
+                        </form>
                     </div>
-                </>
-            ) : null}
-        </>
-    );
-};
+                </div>
+                <div className="flex items-center justify-end p-6 border-t-2 border-solid border-[#9d0000] rounded-b">
+                    <button
+                        className="text-white bg-[#9d0000] font-bold uppercase text-sm px-8 py-3 rounded shadow-md hover:shadow-lg hover:opacity-80 outline-none focus:outline-none mr-4 mb-1 transition"
+                        type="button"
+                        onClick={closeBtnHandler}
+                    >
+                        Submit
+                    </button>
+                    <button
+                        className="text-[#9d0000] bg-transparent border border-[#9d0000] font-bold uppercase text-sm px-8 py-3 rounded shadow-md hover:shadow-lg hover:opacity-80 outline-none focus:outline-none mr-1 mb-1 transition"
+                        type="button"
+                        onClick={closeBtnHandler}
+                    >
+                        Close
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
 
 export default Modal;
