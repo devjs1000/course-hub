d3945d52f7f8d8460b8bcd5b2f5a431cb9cd0850
mohit <mohitdalphonso@gmail.com>
2022-03-02 18:01:15 +0530

    custom hook for intersection api

---
+++ src/UI/useIntersection.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect, useRef, useMemo } from 'react';
+
+const useIntersection = (targetRef, options) => {
+	const [isVisible, setIsVisible] = useState();
+
+	const callbackFunction = entries => {
+		const entry = entries[0];
+		setIsVisible(entry.isIntersecting);
+	};
+
+	const optionsObject = useMemo(() => {
+		return options;
+	}, []);
+
+	useEffect(() => {
+		const observer = new IntersectionObserver(callbackFunction, optionsObject);
+		const target = targetRef.current;
+		if (target) observer.observe(target);
+
+		return () => {
+			if (target) observer.unobserve(target);
+		};
+	}, [targetRef, optionsObject]);
+
+	return isVisible;
+};
+
+export default useIntersection;
