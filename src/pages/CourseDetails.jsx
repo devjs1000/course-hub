5e0fd4cb572ac7ec946e009f8df0fa2472826acd
mohit <mohitdalphonso@gmail.com>
2022-03-03 18:03:16 +0530

    bg color override with white background

--- src/pages/CourseDetails.jsx
+++ src/pages/CourseDetails.jsx
@@ -23,7 +23,7 @@ const CourseDetails = () => {
 		<>
 			<HeroSection course={current} />
 			<DetailsNavigation />
-			<div className="px-4 py-6 grid grid-cols-1 text-gray-600 lg:grid-cols-6 lg:px-16 lg:py-8 lg:gap-2">
+			<div className="px-4 py-6 grid grid-cols-1 text-gray-600 bg-white lg:grid-cols-6 lg:px-16 lg:py-8 lg:gap-2">
 				<div className="col-span-4 order-2 p-2">
 					<DetailsAbout />
 					<AuthorIntro />
