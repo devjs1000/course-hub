227f4543091309e28b63bad91a1794f16934577a
mohit <mohitdalphonso@gmail.com>
2022-03-05 22:39:09 +0530

    tag color changes based on prop

--- src/components/profile/OngoingCourseCard.jsx
+++ src/components/profile/OngoingCourseCard.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 
 function OngoingCourseCard({ tag, title }) {
+	let tagGradient;
+
+	if (tag === 'frontend') {
+		tagGradient = 'from-yellow-500 to-yellow-300';
+	} else if (tag === 'backend') {
+		tagGradient = 'from-green-500 to-green-300';
+	} else if (tag === 'database') {
+		tagGradient = 'from-orange-500 to-orange-300';
+	} else if (tag === 'fullstack') {
+		tagGradient = 'from-blue-500 to-blue-300';
+	}
+
+	const tagClasses = `text-center bg-gradient-to-r ${tagGradient} rounded-md text-white p-1`;
+
 	return (
-		<div className="shadow-md h-[10rem] py-2 px-4 rounded-md bg-white flex flex-col gap-2">
-			<div className="text-center bg-orange-500 rounded-md text-white">
-				{tag}
-			</div>
+		<div className="shadow-md p-4 rounded-md bg-white flex flex-col gap-4 h-[13rem]">
+			<div className={tagClasses}>{tag}</div>
 			<h4 className="text-md font-semibold">{title}</h4>
 			<div className="flex items-center gap-4 mt-auto">
 				<p>Instructor</p>
