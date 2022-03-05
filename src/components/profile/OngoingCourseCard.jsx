b117afecd56b47e1a215e9e989eb53e397e26743
mohit <mohitdalphonso@gmail.com>
2022-03-05 21:08:20 +0530

    card styles changed

--- src/components/profile/OngoingCourseCard.jsx
+++ src/components/profile/OngoingCourseCard.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 
 function OngoingCourseCard({ tag, title }) {
 	return (
-		<div className="shadow-md h-[10rem] py-2 px-4 rounded-md bg-white">
+		<div className="shadow-md h-[10rem] py-2 px-4 rounded-md bg-white flex flex-col gap-2">
 			<div className="text-center bg-orange-500 rounded-md text-white">
 				{tag}
 			</div>
 			<h4 className="text-md font-semibold">{title}</h4>
+			<div className="flex items-center gap-4 mt-auto">
+				<p>Instructor</p>
+				<progress className="h-2 rounded-full"></progress>
+			</div>
 		</div>
 	);
 }
