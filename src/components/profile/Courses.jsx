5e710c7ad9c385d49fde45afc6f61484eda7c340
mohit <mohitdalphonso@gmail.com>
2022-03-05 21:09:47 +0530

    UI updated

--- src/components/profile/Courses.jsx
+++ src/components/profile/Courses.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import OngoingCourseCard from './OngoingCourseCard';
 
 function Courses() {
-	return <div>Courses</div>;
+	return (
+		<div className="px-12 grid grid-cols-4 gap-4">
+			<OngoingCourseCard tag="frontend" title="Beginner to Advanced ReactJS" />
+			<OngoingCourseCard tag="frontend" title="Beginner to Advanced ReactJS" />
+			<OngoingCourseCard tag="frontend" title="Beginner to Advanced ReactJS" />
+			<OngoingCourseCard tag="frontend" title="Beginner to Advanced ReactJS" />
+		</div>
+	);
 }
 
 export default Courses;
