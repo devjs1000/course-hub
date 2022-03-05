dc6ce0a8364389d699fe12b062bc798add9862ee
mohit <mohitdalphonso@gmail.com>
2022-03-05 22:38:04 +0530

    courses card UI updated

--- src/components/profile/Courses.jsx
+++ src/components/profile/Courses.jsx
@@ -5,9 +5,9 @@ function Courses() {
 	return (
 		<div className="px-12 grid grid-cols-4 gap-4">
 			<OngoingCourseCard tag="frontend" title="Beginner to Advanced ReactJS" />
-			<OngoingCourseCard tag="frontend" title="Beginner to Advanced ReactJS" />
-			<OngoingCourseCard tag="frontend" title="Beginner to Advanced ReactJS" />
-			<OngoingCourseCard tag="frontend" title="Beginner to Advanced ReactJS" />
+			<OngoingCourseCard tag="backend" title="Beginner to Advanced NodeJS" />
+			<OngoingCourseCard tag="database" title="Beginner to Advanced MongoDB" />
+			<OngoingCourseCard tag="fullstack" title="Beginner to Advanced MERN" />
 		</div>
 	);
 }
