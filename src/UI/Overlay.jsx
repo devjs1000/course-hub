5bb588d478d8e0f00f903210bb4aba6852c6f3a1
mohit <mohitdalphonso@gmail.com>
2022-03-06 19:27:40 +0530

    made component re-usable

--- src/UI/Overlay.jsx
+++ src/UI/Overlay.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
-function Overlay({ openSidebarHandler }) {
-	return <div className="overlay" onClick={openSidebarHandler}></div>;
+function Overlay({ clickHandler }) {
+	return (
+		<div
+			className="fixed top-0 left-0 w-full h-screen bg-[rgba(0,0,0,.7)] z-50"
+			onClick={clickHandler}
+		></div>
+	);
 }
 
 export default Overlay;
