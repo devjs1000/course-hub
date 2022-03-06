12185e7854cc108f1fdab7661279fe7935111062
mohit <mohitdalphonso@gmail.com>
2022-03-06 19:27:11 +0530

    new section for course video is added

---
+++ src/components/CourseVideo.jsx
@@ -0,0 +1,9 @@
+function CourseVideo() {
+	return (
+		<div className="fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] h-[80vh] bg-white w-[75%] z-50">
+			Video
+		</div>
+	);
+}
+
+export default CourseVideo;
