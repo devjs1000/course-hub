74c2b1231c4545b7d6cec720f733b7c020ff6c89
mohit <mohitdalphonso@gmail.com>
2022-03-07 22:41:07 +0530

    mapping over an array to render next lesson video

--- src/components/CourseVideo.jsx
+++ src/components/CourseVideo.jsx
@@ -8,14 +8,31 @@ import {
 import videoMp4 from '../images/video.mp4';
 import videoWebm from '../images/video.webm';
 
-const NextLesson = () => {
+import videoMp42 from '../images/video2.mp4';
+import videoWebm2 from '../images/video2.webm';
+
+// for testing only start
+const nextLessonVideos = [
+	{ title: 'Lesson 1', videoLength: 11.3, src1: videoMp42, src2: videoWebm2 },
+	{ title: 'Lesson 2', videoLength: 16.2, src1: videoMp4, src2: videoWebm },
+	{ title: 'Lesson 3', videoLength: 18.0, src1: videoMp42, src2: videoWebm2 },
+	{ title: 'Lesson 4', videoLength: 22.23, src1: videoMp4, src2: videoWebm },
+];
+// for testing only end
+
+const NextLesson = ({ title, videoLength, src1, src2, onClickHandler }) => {
 	return (
-		<div className="flex items-center gap-10 px-2 py-1 rounded-md odd:bg-gray-50 even:bg-gray-100 cursor-pointer hover:bg-white">
-			<div className="h-12 w-12 rounded-full bg-red-100 grid place-content-center border border-red-200">
+		<div
+			className="flex items-center gap-10 px-2 py-1 rounded-md odd:bg-gray-50 even:bg-gray-100 cursor-pointer hover:bg-white"
+			data-src1={src1}
+			data-src2={src2}
+			onClick={onClickHandler}
+		>
+			<span className="h-12 w-12 rounded-full bg-red-100 grid place-content-center border border-red-200">
 				<PlayFill className="text-3xl text-primary-color-light" />
-			</div>
-			<h6 className="text-2xl">Lesson2</h6>
-			<span className="ml-auto">12:20</span>
+			</span>
+			<h6 className="text-base font-semibold lg:text-lg">{title}</h6>
+			<span className="text-sm ml-auto">{videoLength.toFixed(2)}</span>
 		</div>
 	);
 };
@@ -40,6 +57,8 @@ function CourseVideo({ clickHandler }) {
 		target.current.currentTime -= 10;
 	};
 
+	const onNextVideoPlayHandler = e => {};
+
 	const controlsClasses = `absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex items-center justify-evenly text-5xl text-gray-200 w-full transition-all duration-300 ${
 		cursorOnVideo ? 'opacity-100' : 'opacity-0'
 	}`;
@@ -51,7 +70,7 @@ function CourseVideo({ clickHandler }) {
 		'text-[10px] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] select-none';
 
 	return (
-		<div className="fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] h-[100vh] bg-white w-[55%] z-50 p-4 flex flex-col gap-4">
+		<div className="fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] h-[100vh] bg-white w-[95%] z-50 p-4 flex flex-col gap-4 md:w-[70%] lg:w-[55%]">
 			<X
 				className="text-6xl self-end text-primary-color-light hover:text-primary-color-dark cursor-pointer"
 				onClick={clickHandler}
@@ -81,11 +100,23 @@ function CourseVideo({ clickHandler }) {
 				{/* video end */}
 			</div>
 			<div className=" overflow-y-scroll flex flex-col gap-2">
-				<NextLesson />
-				<NextLesson />
-				<NextLesson />
-				<NextLesson />
-				<NextLesson />
+				{nextLessonVideos.map(lesson => {
+					return (
+						<NextLesson
+							key={Math.random().toString()}
+							title={lesson.title}
+							videoLength={lesson.videoLength}
+							src1={lesson.src1}
+							src2={lesson.src2}
+							onClickHandler={onNextVideoPlayHandler}
+						/>
+					);
+				})}
+				{/* <NextLesson onClickHandler={onNextVideoPlayHandler} />
+				<NextLesson onClickHandler={onNextVideoPlayHandler} />
+				<NextLesson onClickHandler={onNextVideoPlayHandler} />
+				<NextLesson onClickHandler={onNextVideoPlayHandler} />
+				<NextLesson onClickHandler={onNextVideoPlayHandler} /> */}
 			</div>
 		</div>
 	);
