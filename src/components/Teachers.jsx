1f0a85aa3abfff17c1b232a728ff9e0b04db8972
mohit <mohitdalphonso@gmail.com>
2022-03-08 21:30:38 +0530

    code modified with re-usable compoenent

--- src/components/Teachers.jsx
+++ src/components/Teachers.jsx
@@ -4,6 +4,7 @@ import instOneImage from '../images/instructors/Inst-1.jpg';
 import instTwoImage from '../images/instructors/Inst-2.jpg';
 import instThreeImage from '../images/instructors/Inst-3.jpg';
 import instFourImage from '../images/instructors/Inst-4.jpg';
+import SectionHeading from '../UI/SectionHeading';
 
 const InstructorCard = ({ image }) => {
 	return (
@@ -37,14 +38,10 @@ const InstructorCard = ({ image }) => {
 function Teachers() {
 	return (
 		<div className="bg-[#fffdfd] text-slate-900 flex flex-col items-start px-16 py-16">
-			<div className="w-[50%] flex flex-col gap-10">
-				<h4 className="max-w-min text-slate-400 text-2xl font-semibold relative after:absolute after:bottom-[-1rem] after:left-0 after:h-1 after:w-[40%] after:bg-primary-color-light">
-					Instructors
-				</h4>
-				<h2 className="text-5xl font-semibold leading-[3.5rem]">
-					Our professional & Expert Course Instructors
-				</h2>
-			</div>
+			<SectionHeading
+				subHeading="Instructors"
+				heading="Our professional & Expert Course Instructors"
+			/>
 			<div className="w-full grid grid-cols-2 gap-6 mt-16">
 				<InstructorCard image={instOneImage} />
 				<InstructorCard image={instTwoImage} />
