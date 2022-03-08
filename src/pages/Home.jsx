72533eccd8294780b9d0eaf7a36e5f9471ee989b
mohit <mohitdalphonso@gmail.com>
2022-03-08 21:31:07 +0530

    Why section is created in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Header from '../components/header/Header';
 import Courses from '../components/courses/Courses';
 import Teachers from '../components/Teachers';
+import WhyUs from '../components/WhyUs';
 
 const Home = () => {
 	return (
 		<>
 			<Header />
+			<WhyUs />
 			<Courses />
 			<Teachers />
 		</>
