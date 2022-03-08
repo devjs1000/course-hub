754cb306f8358c3f777c2f00f21d31dceba36b7d
mohit <mohitdalphonso@gmail.com>
2022-03-08 23:05:54 +0530

    button color changed

--- src/UI/Button.jsx
+++ src/UI/Button.jsx
@@ -14,9 +14,9 @@ const Button = ({
 		isWidthFull ? 'w-full' : 'w-[8rem]'
 	} h-[2.5rem] font-semibold text-base rounded-md hover:opacity-80 sm:text-lg cursor-pointer sm:w-[10rem] h-[3rem]`;
 	const primaryBtnStyles = `${buttonCommonStyles} bg-primary-color-light text-white`;
-	const secondaryBtnStyles = `${buttonCommonStyles} bg-white text-primary-color-dark`;
-	const secondaryOutline = `${buttonCommonStyles} border-2 border-primary-color-dark ${
-		textPrimary && 'text-primary-color-dark'
+	const secondaryBtnStyles = `${buttonCommonStyles} bg-white text-primary-color-light`;
+	const secondaryOutline = `${buttonCommonStyles} border-2 border-primary-color-light ${
+		textPrimary && 'text-primary-color-light'
 	} ${textWhite && 'text-white'}`;
 
 	const btnStyle = isPrimary
