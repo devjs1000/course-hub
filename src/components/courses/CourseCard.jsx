331db9b7a823e18c4e95c808691ff0d79d3bf796
mohit <mohitdalphonso@gmail.com>
2022-03-09 16:33:37 +0530

    layout modified

--- src/components/courses/CourseCard.jsx
+++ src/components/courses/CourseCard.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import { Link } from 'react-router-dom';
 import useStore from '../../context/useStore';
+import author from '../../images/author.jpg';
+import { Clock, ListUl } from 'react-bootstrap-icons';
 
 const CourseCard = ({
 	enrolled = false,
@@ -22,60 +24,51 @@ const CourseCard = ({
 		setCurrent(data);
 	}, [id]);
 
-	const bluePrint = {
-		enterline: {
-			enrolled: 'Start Course',
-			noEnrolled: 'See More',
-		},
-	};
-
 	return (
 		<ErrorBoundary fallback={'error in course page'}>
-			<div className="rounded-sm relative max-w-[18rem] shadow-md overflow-hidden">
-				<div className="h-[10rem] rounded-t-sm overflow-hidden relative before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-[rgba(0,0,0,.1)]">
+			<div className="rounded-sm relative w-[21rem] shadow-md overflow-hidden">
+				<div className="h-[12rem] rounded-t-sm overflow-hidden relative before:absolute before:top-0 before:left-0 before:w-full before:h-full before:bg-[rgba(255,118,118,0.09)]">
 					<img
 						src={drill ? image : current?.image}
 						alt="course-image"
 						className="object-cover w-full h-full"
 					/>
 				</div>
-				<div className="px-4 py-10 bg-white text-slate-900 flex flex-col items-start gap-4">
-					<span className="bg-gray-400 text-[10px] font-semibold rounded-sm text-white px-2 py-[.1rem] uppercase flex-end">
+				<div className="relative px-8 py-10 bg-white text-slate-900 flex flex-col items-start gap-4">
+					<span className="bg-orange-300 text-[10px] font-semibold rounded-sm text-white px-2 py-[.1rem] uppercase flex-end">
 						{drill ? type : current?.type}
 					</span>
-					<h3 className="leading-6 text-lg">
-						<span className="text-[1.6rem] font-semibold block">
-							{drill ? title : current?.title}
-						</span>
-						<span className="font-semibold text-slate-800 block truncate h-[1.8rem] w-[calc(100%-3rem)] ">
-							{drill ? tagline : current?.tagline}
-						</span>
+					<div className="flex items-center gap-6">
+						<div className="flex items-center gap-2 font-semibold text-gray-700">
+							<Clock className="text-lg text-primary-color-light" />
+							<span>35 Hours</span>
+						</div>
+						<div className="flex items-center gap-2 font-semibold text-gray-700">
+							<ListUl className="text-xl text-primary-color-light" />
+							<span>300</span>
+						</div>
+					</div>
+					<h3 className="leading-6 text-xl font-semibold h-12 text-slate-800">
+						{drill ? tagline : current?.tagline}
 					</h3>
-
-					<p className="overflow-auto text-lg absolute opacity h-[10rem] w-[100%] top-[0rem] rounded-t-sm hover:text-white p-4 left-[0rem] text-transparent select-none hover:bg-red-300">
-						{drill ? description : current?.description}
-					</p>
+					<div className="flex items-center gap-4">
+						<div
+							className="bg-cover h-12 w-12 rounded-full border bg-no-repeat bg-top"
+							style={{ backgroundImage: `url(${author})` }}
+						></div>
+						<span>John Doe</span>
+					</div>
 					<div className="flex items-center justify-between w-full">
-						{enrolled ? (
-							<Link
-								to="/"
-								className="block bg-primary-color-light text-white py-1 px-2 font-semibold rounded-sm hover:px-4 transition-all"
-							>
-								{bluePrint?.enterline?.enrolled} &#8594;
-							</Link>
-						) : (
-							<>
-								<Link
-									to={`coursedetails/${id}`}
-									className="block bg-primary-color-light text-white py-1 px-2 font-semibold rounded-sm hover:px-4 transition-all"
-								>
-									{bluePrint?.enterline?.noEnrolled} &#8594;
-								</Link>
-								<span className="text-xl">
-									₹ {drill ? price : current?.price}/-{' '}
-								</span>
-							</>
-						)}
+						<Link
+							to={`coursedetails/${id}`}
+							className="block mt-2 bg-primary-color-light text-white py-[.35rem] px-3 font-semibold rounded-sm hover:bg-red-500 transition-all"
+						>
+							See More &#8594;
+						</Link>
+
+						<span className="absolute top-[-1.1rem] right-[1rem] bg-[#fc2340] px-4 py-1 rounded-sm text-white text-xl">
+							₹ {drill ? price : current?.price}/-{' '}
+						</span>
 					</div>
 				</div>
 			</div>
