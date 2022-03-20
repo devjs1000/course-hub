996f5109bdcf4eb6b6e3d7a91cb6ecdad1f26668
anand <atim4077@gmail.com>
2022-03-21 04:51:06 +0530

    assignment error fixed and minor bug fixed

--- src/components/profile/Dashboard.jsx
+++ src/components/profile/Dashboard.jsx
@@ -1,27 +1,31 @@
 import currentCourseImg from '../../images/course-current.jpeg';
 import Button from '../../UI/Button';
-import dashboard from '../../images/dashboard.svg';
+import dashboardImg from '../../images/dashboard.svg';
 import React, {useContext} from 'react';
 import {GlobalContext} from '../../components/DarkMode/ThemeContext'
 import NextVideo from './byte-components/NextVideo';
+import useStore from '../../context/useStore';
 
 function Dashboard() {
 	//styling for dark mode --cjreads665
 	const {theme} = useContext(GlobalContext)
+	const {user}=useStore()
 	const h4Style = `text-xl mb-3 font-semibold`
 	const courseTitle = `font-semibold text-black`
+	console.log(dashboardImg);
+
 	return (
 		<div className="px-4 w-full lg:px-16">
 			<div className="relative bg-[#FFBF98] w-full rounded-lg flex flex-col items-start justify-center gap-4 px-4 py-8 lg:px-16">
 				<h2 className="text-4xl text-primary-color-light">
-					Welcome back, Adnan
+					Welcome back, {user?.name}
 				</h2>
 				<p className="text-lg">
 					Education is the passport to the future So, Learn more & more
 				</p>
 				<Button isPrimary={true}>Browse</Button>
 				<img
-					src={dashboard}
+					src={dashboardImg}
 					alt="dashboard svg"
 					className="opacity-0 absolute w-[35%] right-0 top-[50%] translate-y-[-50%] lg:opacity-100"
 				/>
--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,161 +1,158 @@
-import { useState, useEffect, Children, useContext } from 'react';
-import LongButton from '../../UI/LongButton';
+import { useState, Children, useContext } from "react";
 import {
-	Pen,
-	Journal,
-	JournalCode,
-	Gear,
-	Bell,
-	Speedometer,
-	Search,
-	BoxArrowLeft,
-	List,
-} from 'react-bootstrap-icons';
-import { Link, Outlet } from 'react-router-dom';
-import useStore from '../../context/useStore';
-import useAuthHook from '../../hooks/useAuthHook';
-import { profileDesign } from '../../styles/styleObjects';
-import {GlobalContext} from '../../components/DarkMode/ThemeContext'
-
+  Journal,
+  JournalCode,
+  Gear,
+  Bell,
+  Speedometer,
+  Search,
+  BoxArrowLeft,
+  List,
+} from "react-bootstrap-icons";
+import { Link, Outlet } from "react-router-dom";
+import useStore from "../../context/useStore";
+import useAuthHook from "../../hooks/useAuthHook";
+import { GlobalContext } from "../../components/DarkMode/ThemeContext";
 
 const Profile = () => {
-	const [openAside, setOpenAside] = useState(false);
-	const [showNotificationCard, setShowNotificationCard] = useState(false);
-	const { user, myCourses, setUser } = useStore();
-	const [isTeacher, setIsTeacher] = useState(user.isInstructor);
-	console.log(myCourses);
-	const { logout } = useAuthHook();
-	const joining = new Date(Date.parse(user.createdAt)).toDateString();
-	//added variables here - cjreads665
-	const {theme} = useContext(GlobalContext)
-	const navBarClasses = `relative h-14 ${theme ? 'bg-slate-800  text-white' : 'bg-white text-gray-600'} flex items-center justify-between px-4 border-l lg:px-16`
-	const searchBarClasses = `border h-[80%] flex items-center w-[40%] rounded-md overflow-hidden`
-	const containerClasses = `${theme ? 'bg-slate-800  text-white' : 'bg-white text-gray-600'} flex`
-	useEffect(() => {
-		setIsTeacher(user.isInstructor);
-	}, [user]);
+  const [openAside, setOpenAside] = useState(false);
+  const [showNotificationCard, setShowNotificationCard] = useState(false);
+  const { user, myCourses } = useStore();
 
-	const toggleAsideHandler = () => {
-		setOpenAside(!openAside);
-	};
+  console.log(myCourses);
+  const { logout } = useAuthHook();
+  const joining = new Date(Date.parse(user.createdAt)).toDateString();
+  //added variables here - cjreads665
+  const { theme } = useContext(GlobalContext);
+  const navBarClasses = `relative h-14 ${
+    theme ? "bg-slate-800  text-white" : "bg-white text-gray-600"
+  } flex items-center justify-between px-4 border-l lg:px-16`;
+  const searchBarClasses = `border h-[80%] flex items-center w-[40%] rounded-md overflow-hidden`;
+  const containerClasses = `${
+    theme ? "bg-slate-800  text-white" : "bg-white text-gray-600"
+  } flex`;
 
-	const notificationToggleHandler = () => {
-		setShowNotificationCard(!showNotificationCard);
-	};
-	const handleLogout=()=>{
-		setUser({})
-	}
+  const toggleAsideHandler = () => {
+    setOpenAside(!openAside);
+  };
 
-	const sidebarItems = [
-		{ name: 'Dashboard', icon: <Speedometer />, path: '/my-profile/dashboard' },
-		{ name: 'My Courses', icon: <Journal />, path: '/my-profile/courses' },
-		{
-			name: 'Assignments',
-			icon: <JournalCode />,
-			path: '/my-profile/assignments',
-		},
-		{
-			name: 'Notifications',
-			icon: <Bell />,
-			path: '/my-profile/notifications',
-		},
-		{ name: 'Settings', icon: <Gear />, path: '/my-profile/settings' },
-	];
-{/*added conditional here - cjreads665*/}
+  const notificationToggleHandler = () => {
+    setShowNotificationCard(!showNotificationCard);
+  };
 
-	const asideClasses = `sticky top-0  shadow-xl h-screen ${
-		openAside ? 'px-4 py-2 w-[35vh]' : 'w-0'
-	} ${theme? 'bg-slate-800 text-white' : 'bg-white text-gray-600'} transition-all duration-300 lg:w-[35vh] lg:px-4 lg:py-2`;
+  const sidebarItems = [
+    { name: "Dashboard", icon: <Speedometer />, path: "/my-profile/dashboard" },
+    { name: "My Courses", icon: <Journal />, path: "/my-profile/courses" },
+    {
+      name: "Assignments",
+      icon: <JournalCode />,
+      path: "/my-profile/assignments",
+    },
+    {
+      name: "Notifications",
+      icon: <Bell />,
+      path: "/my-profile/notifications",
+    },
+    { name: "Settings", icon: <Gear />, path: "/my-profile/settings" },
+  ];
+  {
+    /*added conditional here - cjreads665*/
+  }
 
-	const notificationCardClasses = `absolute overflow-hidden right-[5rem] top-[4rem] w-[20rem] rounded-md shadow-xl bg-white ${
-		showNotificationCard ? 'opacity-100' : 'opacity-0'
-	} z-50 transition-all duration-300`;
-//bg-white flex
-	return (
-		<>
-	{/*the right section with the real content*/}
-			<div className={containerClasses}>
-				<aside className={asideClasses}>
-					<Link to="/">
-						<h4 className="text-2xl font-semibold cursor-pointer">
-							XcitEducation
-						</h4>
-					</Link>
-					<ul className="mt-10 flex flex-col gap-12">
-						{Children.toArray(
-							sidebarItems.map(item => {
-								return (
-									<li className="flex items-center gap-3">
-										<span className="text-2xl">{item.icon}</span>
-										<Link to={item.path} className="text-xl">
-											{item.name}
-										</Link>
-									</li>
-								);
-							}),
-						)}
-							<li className="flex items-center gap-3">
-										<span className="text-2xl"><BoxArrowLeft /></span>
-										<button onClick={handleLogout} className="text-xl">
-											Logout?
-										</button>
-									</li>
-						
-					</ul>
-				</aside>
+  const asideClasses = `sticky top-0  shadow-xl h-screen ${
+    openAside ? "px-4 py-2 w-[35vh]" : "w-0"
+  } ${
+    theme ? "bg-slate-800 text-white" : "bg-white text-gray-600"
+  } transition-all duration-300 lg:w-[35vh] lg:px-4 lg:py-2`;
 
+  const notificationCardClasses = `absolute overflow-auto max-h-[10rem] right-[5rem] top-[4rem] w-[20rem] rounded-md shadow-xl bg-white ${
+    showNotificationCard ? "opacity-100" : "opacity-0"
+  } z-50 transition-all duration-300`;
+  //bg-white flex
+  return (
+    <>
+      {/*the right section with the real content*/}
+      <div className={containerClasses}>
+        <aside className={asideClasses}>
+          <Link to="/">
+            <h4 className="text-2xl font-semibold cursor-pointer">
+              XcitEducation
+            </h4>
+          </Link>
+          <ul className="mt-10 flex flex-col gap-12">
+            {Children.toArray(
+              sidebarItems.map((item) => {
+                return (
+                  <li className="flex items-center gap-3">
+                    <span className="text-2xl">{item.icon}</span>
+                    <Link to={item.path} className="text-xl">
+                      {item.name}
+                    </Link>
+                  </li>
+                );
+              })
+            )}
+            <li className="flex items-center gap-3">
+              <span className="text-2xl">
+                <BoxArrowLeft />
+              </span>
+              <button onClick={logout} className="text-xl">
+                Logout?
+              </button>
+            </li>
+          </ul>
+        </aside>
 
-				<main className="w-full flex flex-col gap-6" >
-			{/*the navbar --cjreads665 */}
-					<nav className={navBarClasses}>
-						<List
-							className="visible opacity-100 text-4xl cursor-pointer lg:hidden lg:opacity-0"
-							onClick={toggleAsideHandler}
-						/>
-						<div className={searchBarClasses}>
-							<input
-								type="search"
-								className="flex-[0.9] h-full px-4 border-r bg-gray-50 hover:bg-white focus:bg-white focus:outline-none"
-								placeholder="Search Here"
-							/>
-							<Search className="flex-[0.1] text-2xl w-full px-4 font-semibold cursor-pointer" />
-						</div>
-						<div className="flex items-center gap-6">
-							<Bell
-								className="text-xl cursor-pointer"
-								onClick={notificationToggleHandler}
-							/>
-							<div
-								className="h-10 w-10 rounded-full border cursor-pointer bg-cover bg-center"
-								style={{ backgroundImage: `url(${user.profilePicture})` }}
-							></div>
-						</div>
-						<div className={notificationCardClasses}>
-							<h4 className="bg-gray-50 p-2 font-semibold text-center">
-								Notifications
-							</h4>
-							<div className="flex flex-col gap-1">
-								{/* notification start */}
-								<div className="relative px-4 py-1 before:absolute before:top-0 before:left-0 before:h-full before:w-1 before:bg-red-400">
-									Lorem ipsum dolor sit amet.
-								</div>
-								<div className="relative px-4 py-1 before:absolute before:top-0 before:left-0 before:h-full before:w-1 before:bg-green-400">
-									Lorem ipsum dolor sit amet.
-								</div>
-								<div className="relative px-4 py-1 before:absolute before:top-0 before:left-0 before:h-full before:w-1 before:bg-red-400">
-									Lorem ipsum dolor sit amet.
-								</div>
-								{/* notification end */}
-							</div>
-						</div>
-					</nav>
-					<Outlet />
-				</main>
-			</div>
-			{/*  */}
-			
-		</>
-	);
+        <main className="w-full flex flex-col gap-6">
+          {/*the navbar --cjreads665 */}
+          <nav className={navBarClasses}>
+            <List
+              className="visible opacity-100 text-4xl cursor-pointer lg:hidden lg:opacity-0"
+              onClick={toggleAsideHandler}
+            />
+            <div className={searchBarClasses}>
+              <input
+                type="search"
+                className="flex-[0.9] h-full px-4 border-r bg-gray-50 hover:bg-white focus:bg-white focus:outline-none"
+                placeholder="Search Here"
+              />
+              <Search className="flex-[0.1] text-2xl w-full px-4 font-semibold cursor-pointer" />
+            </div>
+            <div className="flex items-center gap-6">
+              <Bell
+                className="text-xl cursor-pointer"
+                onClick={notificationToggleHandler}
+              />
+              <div
+                className="h-10 w-10 rounded-full border cursor-pointer bg-cover bg-center"
+                style={{ backgroundImage: `url(${user.image})` }}
+              ></div>
+            </div>
+            <div className={notificationCardClasses}>
+              <h4 className="bg-gray-50 p-2 font-semibold text-center">
+                Notifications
+              </h4>
+              <div className="flex flex-col gap-1">
+                {/* notification start */}
+                <div className="relative px-4 py-1 before:absolute before:top-0 before:left-0 before:h-full before:w-1 before:bg-red-400">
+                  Lorem ipsum dolor sit amet.
+                </div>
+                <div className="relative px-4 py-1 before:absolute before:top-0 before:left-0 before:h-full before:w-1 before:bg-green-400">
+                  Lorem ipsum dolor sit amet.
+                </div>
+                <div className="relative px-4 py-1 before:absolute before:top-0 before:left-0 before:h-full before:w-1 before:bg-red-400">
+                  Lorem ipsum dolor sit amet.
+                </div>
+                {/* notification end */}
+              </div>
+            </div>
+          </nav>
+          <Outlet />
+        </main>
+      </div>
+      {/*  */}
+    </>
+  );
 };
 
 export default Profile;
