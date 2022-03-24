04fcaeeffb1bedb5f54f0352f5d81e65661d736e
Shahid <shahid.alam57@gmail.com>
2022-03-24 19:48:33 +0530

    global dark mode added

--- src/UI/SectionHeading.jsx
+++ src/UI/SectionHeading.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import useStore from '../context/useStore'
 
 function SectionHeading({ subHeading, heading }) {
+	const {theme} = useStore()
+	const headingStyles = `text-5xl font-semibold leading-[3.5rem] ${theme?'text-white' : 'text-slate-900'}`
 	return (
 		<div className="w-full flex flex-col gap-10 lg:w-[50%]">
 			<h4 className="max-w-min text-slate-400 text-2xl font-semibold relative after:absolute after:bottom-[-1rem] after:left-0 after:h-1 after:w-[40%] after:bg-primary-color-light uppercase">
 				{subHeading}
 			</h4>
-			<h2 className="text-5xl font-semibold leading-[3.5rem] text-slate-900">
+			<h2 className={headingStyles}>
 				{heading}
 			</h2>
 		</div>
--- src/components/WhyUs.jsx
+++ src/components/WhyUs.jsx
@@ -3,15 +3,20 @@ import Button from '../UI/Button';
 import SectionHeading from '../UI/SectionHeading';
 import { Check } from 'react-bootstrap-icons';
 import { Link } from 'react-router-dom';
+import useStore from '../context/useStore'
+
 
 const WhyFeature = ({ title }) => {
+const {theme} = useStore()
+const paraStyles = ` ${theme? 'text-white' : 'text-slate-700'} `
+const titleStyles = `${theme? 'text-white' : 'text-slate-900'} text-2xl font-semibold mb-2`
 	return (
 		<div className="relative">
 			<div className="absolute top-[.2rem] left-[-4rem] w-8 h-8 border rounded-full bg-primary-color-light grid place-content-center">
 				<Check className="text-white text-xl" />
 			</div>
-			<h4 className="text-2xl font-semibold mb-2 text-slate-900">{title}</h4>
-			<p className="text-slate-700">
+			<h4 className={titleStyles}>{title}</h4>
+			<p className={paraStyles} >
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
 				sequi asperiores eius molestias unde accusantium.
 			</p>
@@ -20,14 +25,17 @@ const WhyFeature = ({ title }) => {
 };
 
 function WhyUs() {
+const {theme} = useStore()
+	const whyUsStyles = `about-section ${theme? 'bg-slate-800' : 'bg-white' } py-8 px-8 flex flex-col items-center gap-10 lg:flex-row md:py-16 lg:py-20`
+	const paraStyles = `w-full ${theme? 'text-white': 'text-slate-700'} lg:w-[90%]`
 	return (
 		<section
-			className="about-section py-8 px-8 flex flex-col items-center gap-10 lg:flex-row md:py-16 lg:py-20"
+			className={whyUsStyles}
 			id="about"
 		>
 			<div className="flex-1 flex flex-col items-start gap-4 lg:flex-[0.5]">
 				<SectionHeading subHeading="WhyUs?" heading="Why Learn Here?" />
-				<p className="w-full text-slate-700 lg:w-[90%]">
+				<p className={paraStyles} >
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa eius
 					est ducimus eos, pariatur labore, necessitatibus explicabo repudiandae
 					ipsam cumque quo hic porro, enim at? Corporis, eum quaerat ea
