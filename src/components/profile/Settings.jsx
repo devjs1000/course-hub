061802cff2c1e201f315bdd2142a7fcaa1e4382b
cj <shahid.alam57@gmail.com>
2022-03-24 22:37:41 +0530

    fixed dark mode bug

--- src/components/profile/Settings.jsx
+++ src/components/profile/Settings.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState, useEffect } from "react";
 import useStore from '../../context/useStore'
-
+import Button from './Button'
 function Settings() {
 	const [btnText, setText] = useState('Off')
 	// const [theme, setTheme] = useState(false)
@@ -11,13 +11,14 @@ const {theme, setTheme} = useStore()
 		<div className='p-8'>
 		
 	{/*content container --cjreads665*/}
-	
+		
 			<span className='text-2xl' >Dark Mode:</span>
-			<button className='bg-sky-800 rounded text-white w-1/12 h-10 ml-2' onClick={()=>{
+			{/*<button className='bg-sky-800 rounded text-white w-1/12 h-10 ml-2' onClick={()=>{
 				setTheme(!theme)
 				setText(()=> btnText=='Off'? 'On': 'Off')
 			}}>{btnText}</button>
-	
+			<Button/>*/}
+			<Button/>
 		</div>)
 }
 
