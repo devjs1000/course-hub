05e0ac46ad080be68ebcde1cf8eae829c9917819
anand <atim4077@gmail.com>
2022-03-25 06:32:02 +0530

    dark mode button bug resolved

--- src/components/profile/Button.jsx
+++ src/components/profile/Button.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import useStore from '../../context/useStore'
+import React from "react";
+import useStore from "../../context/useStore";
 import { useState, useEffect } from "react";
 
 const Button = () => {
+  const [btnText, setText] = useState(false);
+  const { theme, setTheme } = useStore();
+  return (
+    <>
+      <input
+        type="checkbox"
+		checked={theme}
+        class="absolute left-1/2 -translate-x-1/2 w-full h-20 peer appearance-none rounded-md"
+        onClick={()=>setTheme(val=>!val)}
+      />
+      <span class="w-16 h-10 flex items-center flex-shrink-0 ml-4 p-1 bg-gray-300 rounded-full duration-300 ease-in-out peer-checked:bg-green-400 after:w-8 after:h-8 after:bg-white after:rounded-full after:shadow-md after:duration-300 peer-checked:after:translate-x-6 group-hover:after:translate-x-1"></span>
+    </>
+  );
+};
 
-	const [btnText, setText] = useState('Off')
-	const {theme, setTheme} = useStore()
-	return (
-		<>
-  <input type="checkbox" class="absolute left-1/2 -translate-x-1/2 w-full h-20 peer appearance-none rounded-md" onClick={(e)=>{
-  	btnText=='Off'? setTheme(!theme): 'Off'
-  }} />
-  <span class="w-16 h-10 flex items-center flex-shrink-0 ml-4 p-1 bg-gray-300 rounded-full duration-300 ease-in-out peer-checked:bg-green-400 after:w-8 after:h-8 after:bg-white after:rounded-full after:shadow-md after:duration-300 peer-checked:after:translate-x-6 group-hover:after:translate-x-1"></span>
-  </>
-
-	)
-}
-
-export default Button
+export default Button;
