a46c489cffb36ac56c17e9c47efc2b492c469cdb
anand <atim4077@gmail.com>
2022-03-28 13:48:38 +0530

    dev mode

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:6900/xcite",
+  uri: "https://xciteserver.herokuapp.com/xcite",
   cache: new InMemoryCache(),
 });
 
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default ()=>{
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:6900/xcite/",
+        target: "https://xciteserver.herokuapp.com/xcite",
         changeOrigin: true,
         secure: false,
         ws: true,
