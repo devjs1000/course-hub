525d37b4d571723624b78f4a3e89320c0f615389
cjreads665 <shahid.alam57@gmail.com>
2022-04-03 20:32:17 +0530

    added useParams for assignments/chapters

--- src/components/Chapters.jsx
+++ src/components/Chapters.jsx
@@ -4,7 +4,7 @@ import {useParams} from 'react-router-dom'
 const Chapters = () => {
 	let {id} = useParams()
 	return (
-		<div className='bg-white h-full'>
+		<div className='bg-white h-32 p-8'>
 			this is the course {id}
 		</div>
 	)
