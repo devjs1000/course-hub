786d51641402716aea87244ee0ce7246f8847690
cjreads665 <shahid.alam57@gmail.com>
2022-04-20 22:10:30 +0530

    added details for homepage

--- src/components/WhyUs.jsx
+++ src/components/WhyUs.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 import useStore from '../context/useStore'
 
 
-const WhyFeature = ({ title }) => {
+const WhyFeature = ({ title ,description }) => {
 const {theme} = useStore()
 const paraStyles = ` ${theme? 'text-white' : 'text-slate-700'} `
 const titleStyles = `${theme? 'text-white' : 'text-slate-900'} text-2xl font-semibold mb-2`
@@ -17,8 +17,7 @@ const titleStyles = `${theme? 'text-white' : 'text-slate-900'} text-2xl font-sem
 			</div>
 			<h4 className={titleStyles}>{title}</h4>
 			<p className={paraStyles} >
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis
-				sequi asperiores eius molestias unde accusantium.
+				{description}
 			</p>
 		</div>
 	);
@@ -28,6 +27,29 @@ function WhyUs() {
 const {theme} = useStore()
 	const whyUsStyles = `about-section ${theme? 'bg-slate-800' : 'bg-white' } py-8 px-8 flex flex-col items-center gap-10 lg:flex-row md:py-16 lg:py-20`
 	const paraStyles = `w-full ${theme? 'text-white': 'text-slate-700'} lg:w-[90%]`
+	let featuresArray=[
+	{
+		'title' : 'Low Cost',
+		'description' : 'We offer premium courses at affordable prices so you can focus more on your studies' ,
+	},
+	{
+		'title' : 'Learn With Experts',
+		'description' : `Our instructors are from premier Institutes like IITs, Nits, Harvard, etc and from 
+		Fortune 500 startups and companies
+		` ,
+
+
+	},
+	{
+		'title' : 'Different Course Variation',
+		'description' : 'Our courses range from basic internet usage to more advance topics such as Web developement, DevOps, AI-ML,etc' ,
+	},
+	]
+
+	let list = featuresArray.map(obj=>{
+			return	<WhyFeature title={obj.title} description={obj.description}/>
+	})
+
 	return (
 		<section
 			className={whyUsStyles}
@@ -36,11 +58,8 @@ const {theme} = useStore()
 			<div className="flex-1 flex flex-col items-start gap-4 lg:flex-[0.5]">
 				<SectionHeading subHeading="WhyUs?" heading="Why Learn Here?" />
 				<p className={paraStyles} >
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa eius
-					est ducimus eos, pariatur labore, necessitatibus explicabo repudiandae
-					ipsam cumque quo hic porro, enim at? Corporis, eum quaerat ea
-					reprehenderit fuga quo saepe sint nisi commodi totam asperiores odit
-					beatae!
+					We offer the best of educational courses at affordable prices! Our instructors
+					are Senior Engineers from Premier Institues and Fortune 500 start-ups and companies.
 				</p>
 				<Link to="/about-us">
 					<Button isPrimary={true}>Discover More</Button>
@@ -48,9 +67,7 @@ const {theme} = useStore()
 			</div>
 
 			<div className="flex-[0.5] flex flex-col gap-6 bor border-l pl-12">
-				<WhyFeature title="Low Cost" />
-				<WhyFeature title="Learn With Experts" />
-				<WhyFeature title="Diffrent Course Variation" />
+				{list}
 			</div>
 		</section>
 	);
