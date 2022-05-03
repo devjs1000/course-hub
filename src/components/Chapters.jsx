61a0286bc852e9ffe42f002afe30eea7ffeb8d05
devjs1000 <atim4077@gmail.com>
2022-05-04 00:25:25 +0530

    assignment ui changed

--- src/components/Chapters.jsx
+++ src/components/Chapters.jsx
@@ -1,197 +1,203 @@
-import React,{useState, useEffect} from 'react'
-import {useParams} from 'react-router-dom'
-import {getChaptersQuery} from '../graphql/Queries'
-import { useQuery,useMutation } from "@apollo/client";
-import useStore from '../context/useStore'
-import Modal from 'react-modal';
-import { v4 as uuidv4 } from 'uuid';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getChaptersQuery } from "../graphql/Queries";
+import { useQuery, useMutation } from "@apollo/client";
+import useStore from "../context/useStore";
+import Modal from "react-modal";
+import { v4 as uuidv4 } from "uuid";
 import { Link } from "react-router-dom";
 import { submitMyProject } from "../graphql/Mutations";
-import toast from 'react-hot-toast';
+import toast from "react-hot-toast";
 
+Modal.setAppElement("#root");
 
-
-Modal.setAppElement('#root');
-
-
-const ConditionalLink = ({ children, to, condition }) => (condition && to)
-      ? <Link to={to}>{children}</Link>
-      : <>{children}</>;
-
+const ConditionalLink = ({ children, to, condition }) =>
+  condition && to ? <Link to={to}>{children}</Link> : <>{children}</>;
 const Chapters = () => {
-	/*
-	
-	id -> courseId
-	chapterId -> chapter ID
-
-	*/
-	const token = localStorage.getItem("accessToken");
-	
-	const [modalData, setModalData] = useState({
-		chapterName : '',
-		chapterId: ''
-	})
-	const [isOpen, setIsOpen] = useState(false)
-	const [chapterList, setChapterList] = useState([]) 
-	let {id} = useParams()
-	let {user,theme,setCurrentCourseId} = useStore();
-	let fileData;
-	const {data:chapters} = useQuery(getChaptersQuery, {
-	variables: {
-		courseId : id,
-	},
-	context : {
-		headers:{
-			Authorization: token
-		}
-	}
-  })
-	const [submitProject] = useMutation(submitMyProject, {
-	variables: {
-	chapterId: modalData.chapterId,
-	  courseId: id,
-	  projectLink: fileData,
-	},
-	context : {
-		headers:{
-			Authorization: token
-		}
-	}});
-
-
-
-//fn to handle file conversion
-const handleSelect= async (e)=>{
-const file=e.target.files[0]
-const reader=new FileReader()
-
-
-reader.onload=(f)=>{
- fileData=f.target.result
- }
- await reader.readAsDataURL(file);
-}
-	
-const handleSubmit = async (e)=>{
-	e.preventDefault()
-	submitProject({
-		headers:{
-        Authorization: token
-      },
-	variables:{
-	  chapterId: modalData.chapterId,
-	  courseId: id,
-	  projectLink: fileData,
-		}
-	}).then(res=>{
-		console.log(res)
-	}).catch(err=>{
-		console.log(err)
-	})
-
-	// console.log(modalData.chapterId)
-	// console.log(id)
-
-	toast.promise(
-  			submitProject({
-		headers:{
-        Authorization: token
-      },
-	variables:{
-	  chapterId: modalData.chapterId,
-	  courseId: id,
-	  projectLink: fileData,
-		}
-	}),{
-     		loading: 'Saving...',
-     		success: <b>Project Submission Successfully!</b>,
-     		error: <b>Project Submission Failed.</b>,
-   	},
-   	 {
-    success: {
-      duration: 4000,
+  const token = localStorage.getItem("accessToken");
+  const [modalData, setModalData] = useState({
+    chapterName: "",
+    chapterId: "",
+  });
+  const [isOpen, setIsOpen] = useState(false);
+  const [chapterList, setChapterList] = useState([]);
+  let { id } = useParams();
+  let { user, theme, setCurrentCourseId } = useStore();
+  let fileData;
+  const { data: chapters } = useQuery(getChaptersQuery, {
+    variables: {
+      courseId: id,
     },
-    error :{
-      duration: 4000,
+    context: {
+      headers: {
+        Authorization: token,
+      },
+    },
+  });
+  const [submitProject] = useMutation(submitMyProject, {
+    variables: {
+      chapterId: modalData.chapterId,
+      courseId: id,
+      projectLink: fileData,
+    },
+    context: {
+      headers: {
+        Authorization: token,
+      },
+    },
+  });
 
+  //fn to handle file conversion
+  const handleSelect = async (e) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = (f) => {
+      fileData = f.target.result;
+    };
+    await reader.readAsDataURL(file);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    submitProject({
+      headers: {
+        Authorization: token,
+      },
+      variables: {
+        chapterId: modalData.chapterId,
+        courseId: id,
+        projectLink: fileData,
+      },
+    })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    // console.log(modalData.chapterId)
+    // console.log(id)
+
+    toast.promise(
+      submitProject({
+        headers: {
+          Authorization: token,
+        },
+        variables: {
+          chapterId: modalData.chapterId,
+          courseId: id,
+          projectLink: fileData,
+        },
+      }),
+      {
+        loading: "Saving...",
+        success: <b>Project Submission Successfully!</b>,
+        error: <b>Project Submission Failed.</b>,
+      },
+      {
+        success: {
+          duration: 4000,
+        },
+        error: {
+          duration: 4000,
+        },
+      }
+    );
+  };
+
+  useEffect(() => {
+    try {
+      setCurrentCourseId(id);
+      localStorage.getItem("currentCourseId")
+        ? localStorage.getItem("currentCourseId")
+        : localStorage.setItem("currentCourseId", id);
+      let arrayOfItems = chapters.chapters.map((obj) => {
+        let id = obj.id;
+        let path = `/students-enrolled/${id}`;
+        console.log(obj);
+        return (
+          <ConditionalLink
+            condition={user.role == "teacher"}
+            to={path}
+            key={uuidv4()}
+          >
+            <div
+              className="border-[1px] rounded shadow-lg  capitalize  border-gray-100 my-4 p-2 cursor-pointer"
+              onClick={() => {
+                setIsOpen(true);
+                setModalData((prevState) => ({
+                  ...prevState,
+                  chapterName: obj.name,
+                  chapterId: obj.id,
+                }));
+              }}
+            >
+              <div className="bg-red-700 text-white font-bold px-4 py-2 rounded">
+                {obj?.name}
+              </div>
+              <div className="text-gray-700 px-4 py-2">{obj?.project}</div>
+              <div className="text-gray-700 text-sm font-light px-4 py-2">
+                {obj?.about}
+              </div>
+            </div>
+          </ConditionalLink>
+        );
+      });
+      setChapterList(arrayOfItems);
+    } catch {
+      setChapterList("loading....");
     }
-  }
- )
-}
+  }, [chapters]);
 
+  let mainDivStyles = `${
+    theme ? "bg-slate-800 text-white" : "bg-white"
+  } min-h-screen p-8`;
+  let modalStyles = `flex w-full h-full justify-center items-center ${
+    theme ? "bg-slate-800 text-white" : "bg-white"
+  }`;
+  return (
+    <div className={mainDivStyles}>
+      <h1 className="text-4xl font-bold">Chapters</h1>
+      {/* the list of chapters*/}
+      <div className="list-decimal text-xl pt-2">{chapterList}</div>
+      <Modal isOpen={isOpen} onRequestClose={() => setIsOpen(false)}>
+        <div className={modalStyles}>
+          <article className=" w-3/4 h-1/2 flex flex-col justify-around">
+            <h2 className="capitalize text-4xl">
+              Uploading for : {modalData.chapterName}
+            </h2>
+            <input
+              type="file"
+              className="border border-1 border-black p-2"
+              onChange={handleSelect}
+            />
+            <div className="flex justify-around">
+              <button
+                onClick={() => setIsOpen(false)}
+                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+              >
+                I'll Do It Later
+              </button>
+              <button
+                onClick={(e) => {
+                  handleSubmit(e);
+                  setTimeout(() => {
+                    setIsOpen(false);
+                  }, 3000);
+                }}
+                className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded h-12"
+              >
+                Submit
+              </button>
+            </div>
+          </article>
+        </div>
+      </Modal>
+    </div>
+  );
+};
 
-	useEffect(() => {
-		try{
-	setCurrentCourseId(id)
-		localStorage.getItem("currentCourseId") ? localStorage.getItem("currentCourseId") : localStorage.setItem("currentCourseId",id)
-		let arrayOfItems = chapters.chapters.map(obj=>{
-			let id=obj.id
-		let path = `/students-enrolled/${id}`
-		return <ConditionalLink condition={user.role=='teacher'} to={path} key={uuidv4()}>
-		<div className=' border-1 border border-black my-4 p-2 cursor-pointer'
-		onClick={()=>{
-		setIsOpen(true)
-		setModalData(prevState=>({
-			...prevState,
-			chapterName : obj.name,
-			chapterId : obj.id
-		}))
-		}}
-		
+export default Chapters;
 
-		>
-		<span className='text-2xl '>{obj?.name}</span> <br/>
-		</div>
-		</ConditionalLink>
-	})
-	setChapterList(arrayOfItems)			
-		}
-catch{
-	setChapterList('loading....')
-}
-
-	}, [chapters])
-
-let mainDivStyles = `${theme ? 'bg-slate-800 text-white' : 'bg-white'} min-h-screen p-8`
-let modalStyles= `flex w-full h-full justify-center items-center ${theme ? 'bg-slate-800 text-white' : 'bg-white'}`
-	return (
-		<div className={mainDivStyles}>
-			<h1 className='text-4xl font-bold'>Chapters</h1>
-		{/* the list of chapters*/}
-			<div className="list-decimal text-xl pt-2">
-				{chapterList}		
-			</div>
-			<Modal 
-			isOpen={isOpen}
-			onRequestClose={()=>setIsOpen(false)}
-			>
-			<div className={modalStyles}>
-				<article className=' w-3/4 h-1/2 flex flex-col justify-around'>
-				<h2 className='capitalize text-4xl'>Uploading for : {modalData.chapterName}</h2>
-				<input type="file" 
-				className='border border-1 border-black p-2' 
-				onChange={handleSelect}
-
-				 />
-				<div className='flex justify-around'>
-				<button onClick={()=>setIsOpen(false)} className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded' >I'll Do It Later</button>
-				<button
-				onClick={(e)=>{
-				handleSubmit(e)
- 		setTimeout(()=>{
- 					
-					setIsOpen(false)
-				},3000)
-				}}
-				 className='bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded h-12'>Submit</button>
-				</div>
-			</article>
-			</div>
-			
-			</Modal>
-		</div>
-
-	)
-}
-
-export default Chapters
--- src/components/CurrentProject.jsx
+++ src/components/CurrentProject.jsx
@@ -1,110 +1,113 @@
-import React,{useState,useEffect} from 'react'
-import { useLocation } from 'react-router-dom'
-import { useQuery,useMutation } from "@apollo/client";
-import {GetAllProjectsByChapterId} from '../graphql/Queries'
-import {checkProjectMutation} from '../graphql/Mutations'
-import { v4 as uuidv4 } from 'uuid';
-import toast from 'react-hot-toast';
-
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { useQuery, useMutation } from "@apollo/client";
+import { GetAllProjectsByChapterId } from "../graphql/Queries";
+import { checkProjectMutation } from "../graphql/Mutations";
+import { v4 as uuidv4 } from "uuid";
+import toast from "react-hot-toast";
 
 const CurrentProject = () => {
-	const [table, setTable] = useState()
-	const location = useLocation()
-	const { chapterId } = location.state
-	const token = localStorage.getItem("accessToken");
-	const {data:chapters,refetch} = useQuery(GetAllProjectsByChapterId, {
-	variables: {
-		chapterId : chapterId,
-	},
-	context : {
-		headers:{
-			Authorization: token
-		}
-	}
-  })
-  	const [checkProject] = useMutation(checkProjectMutation,{
-		onCompleted: refetch,
-		context : {
-		headers:{
-			Authorization: token
-		}
-	}
-	})
+  const [table, setTable] = useState();
+  const location = useLocation();
+  const { chapterId } = location.state;
+  const token = localStorage.getItem("accessToken");
+  const { data: chapters, refetch } = useQuery(GetAllProjectsByChapterId, {
+    variables: {
+      chapterId: chapterId,
+    },
+    context: {
+      headers: {
+        Authorization: token,
+      },
+    },
+  });
 
-	console.log(localStorage.getItem("currentCourseId"))
-		const handleCheck =(projectId,status)=>{
-		let newStatus = !status
-		console.log(newStatus)
-		checkProject({
-			headers:{
-				Authorization: token
-			},
-			variables:{
-				projectId: projectId,
-				projectStatus : newStatus
-			}
-		})
-		toast.promise(
-  	checkProject({
-			headers:{
-				Authorization: token
-			},
-			variables:{
-				projectId: projectId,
-				projectStatus : newStatus
-			}
-		}),
-   {
-     loading: 'Saving...',
-     success: <b>Checking saved!</b>,
-     error: <b>Could not save.</b>,
-   }
- );
-	}
+  const [checkProject] = useMutation(checkProjectMutation, {
+    onCompleted: refetch,
+    context: {
+      headers: {
+        Authorization: token,
+      },
+    },
+  });
 
-	const getData = async()=>{
-		let list = chapters?.getAllProjectsByChapterId.map(obj=>{
-		console.log(obj)
-		return  <tr key={uuidv4()}>
-      <td className='border border-slate-300'>{obj.userId}</td>
-      <td className='border border-slate-300'>
-      <a href={obj.projectLink}>
-      <button className="h-10 px-5 m-2 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">View Project</button>
-      </a>
-</td>
-      <td className='border border-slate-300'><button 
-      className="h-10 px-5 m-2 text-gray-100 transition-colors duration-150 bg-gray-700 rounded-lg focus:shadow-outline hover:bg-gray-800"
-      onClick={()=>handleCheck(obj.id,obj.projectStatus)}
+  console.log(localStorage.getItem("currentCourseId"));
+  const handleCheck = (projectId, status) => {
+    let newStatus = !status;
+    console.log(newStatus);
+    checkProject({
+      headers: {
+        Authorization: token,
+      },
+      variables: {
+        projectId: projectId,
+        projectStatus: newStatus,
+      },
+    });
+    toast.promise(
+      checkProject({
+        headers: {
+          Authorization: token,
+        },
+        variables: {
+          projectId: projectId,
+          projectStatus: newStatus,
+        },
+      }),
+      {
+        loading: "Saving...",
+        success: <b>Checking saved!</b>,
+        error: <b>Could not save.</b>,
+      }
+    );
+  };
 
-      >{
-      	obj.projectStatus? 'Checked' : 'UnChecked'
-      }</button>
-</td>
-    </tr>
-		})
-		setTable(list)
-	}
+  const getData = async () => {
+    let list = chapters?.getAllProjectsByChapterId.map((obj) => {
+      console.log(obj);
+      return (
+        <tr key={uuidv4()}>
+          <td className="border border-slate-300">{obj.userId}</td>
+          <td className="border border-slate-300">
+            <a href={obj.projectLink}>
+              <button className="h-10 px-5 m-2 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">
+                View Project
+              </button>
+            </a>
+          </td>
+          <td className="border border-slate-300">
+            <button
+              className="h-10 px-5 m-2 text-gray-100 transition-colors duration-150 bg-gray-700 rounded-lg focus:shadow-outline hover:bg-gray-800"
+              onClick={() => handleCheck(obj.id, obj.projectStatus)}
+            >
+              {obj.projectStatus ? "Checked" : "UnChecked"}
+            </button>
+          </td>
+        </tr>
+      );
+    });
+    setTable(list);
+  };
 
-	useEffect(() => {
-		getData()
-	}, [chapters])
+  useEffect(() => {
+    getData();
+  }, [chapters]);
 
-	return (
-		<div className=' w-full h-[50rem] flex flex-col items-center'>
-<table className="table-auto">
-  <thead>
-    <tr>
-      <th className='border border-slate-300'>Student ID</th>
-      <th className='border border-slate-300'>Project Link</th>
-      <th className='border border-slate-300'>Project Status</th>
-    </tr>
-  </thead>
-  <tbody>
-    {table}
-  </tbody>
-</table>
-		</div>
-	)
-}
+  return (
+    <div className=" w-full h-[50rem] flex flex-col items-center">
+      <table className="table-auto">
+        <thead>
+          <tr>
+            <th className="border border-slate-300">Student ID</th>
+            <th className="border border-slate-300">Project Link</th>
+            <th className="border border-slate-300">Project Status</th>
+          </tr>
+        </thead>
+        <tbody>{table}</tbody>
+      </table>
+    </div>
+  );
+};
 
-export default CurrentProject
+export default CurrentProject;
+
