684bfc51083c0784b618afc99b25bc8eea8363b2
cjreads665 <shahid.alam57@gmail.com>
2022-05-05 10:23:52 +0530

    minor commit

--- src/components/Chapters/CurrentProject.jsx
+++ src/components/Chapters/CurrentProject.jsx
@@ -73,7 +73,7 @@ const handleCheck =(projectId,status)=>{
               <TableCell align="right">{obj.userId}</TableCell>
               <TableCell align="right">
                 <a href={obj.projectLink}>
-                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                 View Project</button>
 
                 </a>
