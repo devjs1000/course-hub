39c12764c541b4352490fec979a31d342b8e6761
cjreads665 <shahid.alam57@gmail.com>
2022-05-05 10:22:04 +0530

    modified table for current chapter

--- src/components/Chapters/CurrentProject.jsx
+++ src/components/Chapters/CurrentProject.jsx
@@ -1,3 +1,147 @@
+import React,{useState,Children} from 'react'
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { useQuery,useMutation } from "@apollo/client";
+import {GetAllProjectsByChapterId,getAllProjectsByCourseId} from '../../graphql/Queries'
+import {checkProjectMutation} from '../../graphql/Mutations'
+import toast from 'react-hot-toast';
+
+
+const CurrentProject = (id) => {
+	let [loading,setLoading] = useState(true)
+	console.log(id)
+	const token = localStorage.getItem("accessToken");
+		const {data,refetch} = useQuery(GetAllProjectsByChapterId,{
+		variables:{
+			chapterId: id.id,
+		},
+		context:{
+			headers:{
+				Authorization:token
+			}
+		}
+	})
+
+  const [checkProject] = useMutation(checkProjectMutation,{
+    onCompleted: refetch,
+    context : {
+    headers:{
+      Authorization: token
+    }
+  }
+  })
+const handleCheck =(projectId,status)=>{
+    let newStatus = !status
+    console.log(newStatus)
+    checkProject({
+      headers:{
+        Authorization: token
+      },
+      variables:{
+        projectId: projectId,
+        projectStatus : newStatus
+      }
+    })
+    toast.promise(
+    checkProject({
+      headers:{
+        Authorization: token
+      },
+      variables:{
+        projectId: projectId,
+        projectStatus : newStatus
+      }
+    }),
+   {
+     loading: 'Saving...',
+     success: <b>Checking saved!</b>,
+     error: <b>Could not save.</b>,
+   }
+ );
+  }
+
+    console.log(data?.getAllProjectsByChapterId)
+    let list=Children.toArray(data?.getAllProjectsByChapterId.map(obj=>{
+     return <TableRow  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">{obj.id}</TableCell>
+              <TableCell align="right">{obj.userId}</TableCell>
+              <TableCell align="right">
+                <a href={obj.projectLink}>
+                <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                View Project</button>
+
+                </a>
+              </TableCell>
+              <TableCell align="right"><button
+      className="h-10 px-5 m-2 text-gray-100 transition-colors duration-150 bg-gray-700 rounded-lg focus:shadow-outline hover:bg-gray-800"
+      onClick={()=>handleCheck(obj.id,obj.projectStatus)}
+      >{
+        obj.projectStatus? 'Checked' : 'UnChecked'
+      }</button></TableCell>
+            </TableRow>
+    }))
+
+
+	return (
+		<div>
+			<TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Project ID</TableCell>
+            <TableCell align="right">Student ID</TableCell>
+            <TableCell align="right">Project Link</TableCell>
+            <TableCell align="right">Project Status</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+            {list}
+
+        </TableBody>
+      </Table>
+    </TableContainer>
+		</div>
+	)
+}
+
+export default CurrentProject
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // import * as React from "react";
 // import PropTypes from "prop-types";
 // import Box from "@mui/material/Box";
@@ -14,7 +158,7 @@
 // import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 // import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
-export default ''
+// export default ''
 
 
 // function Row(props) {
