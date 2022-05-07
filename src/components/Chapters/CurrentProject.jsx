5a6e265884b9caf1328613a4d1d81f472ba98bc6
Shahid <shahid.alam57@gmail.com>
2022-05-07 13:30:39 +0530

    added option to download project

--- src/components/Chapters/CurrentProject.jsx
+++ src/components/Chapters/CurrentProject.jsx
@@ -67,12 +67,18 @@ const handleCheck =(projectId,status)=>{
 
     console.log(data?.getAllProjectsByChapterId)
     let list=Children.toArray(data?.getAllProjectsByChapterId.map(obj=>{
+    fetch(obj.projectLink)
+  .then(res => res.blob())
+  .then(blob => {
+    const file = new File([blob], "File name",{ type: "image/png" })
+    console.log(file)
+  })
      return <TableRow  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">{obj.id}</TableCell>
               <TableCell align="right">{obj.userId}</TableCell>
               <TableCell align="right">
-                <a href={obj.projectLink}>
+                <a download href={obj.projectLink}>
                 <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
                 View Project</button>
 
